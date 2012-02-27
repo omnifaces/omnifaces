cabdec9af139872c742cb0573f4386fe186b7fb6
Arjan Tijms <arjantijms@gmail.com>
2012-02-28 00:30:18 +0100

    Added tag handler for converting a value expression into a method expression, to help passing the latter into a Facelets tag.

---
+++ src/org/omnifaces/el/MethodExpressionValueExpressionAdapter.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.el;
+
+import javax.el.ELContext;
+import javax.el.MethodExpression;
+import javax.el.MethodInfo;
+import javax.el.ValueExpression;
+
+/**
+ * This MethodExpression wraps a ValueExpression. 
+ * <p>
+ * With this wrapper a value expression can be used  where a method expression is expected. 
+ * The return value of the method execution will be the value represented by the value expression.
+ * <p>
+ * Note that any parameters passed into the method expression are ignored  
+ * 
+ * @author Arjan Tijms
+ *
+ */
+public class MethodExpressionValueExpressionAdapter extends MethodExpression {
+
+	private static final long serialVersionUID = 1L;
+	
+	private final ValueExpression valueExpression;
+
+	public MethodExpressionValueExpressionAdapter(ValueExpression valueExpression) {
+		this.valueExpression = valueExpression;
+	}
+	
+	@Override
+	public Object invoke(ELContext context, Object[] params) {
+		return valueExpression.getValue(context);
+	}
+	
+	@Override
+	public MethodInfo getMethodInfo(ELContext context) {				
+		return new MethodInfo(null, valueExpression.getExpectedType(), null);
+	}
+
+	@Override
+	public boolean isLiteralText() {
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return valueExpression.hashCode();
+	}
+	
+	@Override
+	public String getExpressionString() {
+		return valueExpression.getExpressionString();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		
+		if (obj instanceof MethodExpressionValueExpressionAdapter) {
+			return ((MethodExpressionValueExpressionAdapter)obj).getValueExpression().equals(valueExpression);
+		}
+		
+		return false;
+	}
+	
+	public ValueExpression getValueExpression() {
+		return valueExpression;
+	}
+}
---
+++ src/org/omnifaces/facelets/MethodParamHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.facelets;
+
+import java.io.IOException;
+
+import javax.el.MethodExpression;
+import javax.el.ValueExpression;
+import javax.faces.component.UIComponent;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagAttribute;
+import javax.faces.view.facelets.TagConfig;
+import javax.faces.view.facelets.TagHandler;
+
+import org.omnifaces.el.MethodExpressionValueExpressionAdapter;
+
+/**
+ * This handler wraps a value expression that's actually a method expression by another value expression that returns a method expression
+ * that gets the value of first value expression, which as "side-effect" executes the original method expression.
+ * 
+ * <p>
+ * This somewhat over-the-top chain of wrapping is done so a method expression can be passed into a Facelet tag as parameter.
+ * 
+ * @author Arjan Tijms
+ *
+ */
+public class MethodParamHandler extends TagHandler {
+
+	private final TagAttribute name;
+	private final TagAttribute value;
+	
+	public MethodParamHandler(TagConfig config) {
+		super(config);
+		this.name = this.getRequiredAttribute("name");
+		this.value = this.getRequiredAttribute("value");
+	}
+	
+	public void apply(FaceletContext ctx, UIComponent parent) throws IOException {
+		String nameStr = name.getValue(ctx);
+		
+		// The original value expression we get inside the Facelets tag, that's actually the method expression passed-in by the user.
+		ValueExpression valueExpression = value.getValueExpression(ctx, Object.class);
+		
+		// A method expression that wraps the value expression and uses its own invoke method to get the value from the wrapped expression.
+		MethodExpression methodExpression = new MethodExpressionValueExpressionAdapter(valueExpression);
+		
+		// Using the variable mapper so the expression is scoped to the body of the Facelets tag. Since the variable mapper only accepts
+		// value expressions, we once again wrap it by a value expression that directly returns the method expression.
+		ctx.getVariableMapper().setVariable(nameStr, ctx.getExpressionFactory().createValueExpression(methodExpression, MethodExpression.class));	
+	}
+
+}
