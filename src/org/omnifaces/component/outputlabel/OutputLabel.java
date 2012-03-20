6efa1e8a61d480684b0f04b37ac85271421d8ff7
Arjan Tijms <arjantijms@gmail.com>
2012-03-20 23:25:42 +0100

    Added extended OutputLabel component that automatically sets the label attribute of the component identified by its for attribute.

---
+++ src/org/omnifaces/component/outputlabel/OutputLabel.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2012 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.component.outputlabel;
+
+import static org.omnifaces.util.Faces.isPostback;
+import static org.omnifaces.util.Utils.isEmpty;
+
+import javax.el.ValueExpression;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.html.HtmlOutputLabel;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PreRenderViewEvent;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
+
+import org.omnifaces.util.Components;
+import org.omnifaces.util.Faces;
+
+/**
+ * <strong>OutputLabel</strong> is a component that extends the standard {@link HtmlOutputLabel} and provides support for
+ * automatically setting its value as the label of the component identified by its <code>for</code> attribute (if any).
+ * <p>
+ * You can use it the same way as <code>&lt;f:outputLabel&gt;</code>, you only need to change <code>f:</code> into
+ * <code>o:</code>.
+ *
+ * @author Arjan Tijms
+ */
+@FacesComponent(OutputLabel.COMPONENT_TYPE)
+public class OutputLabel extends HtmlOutputLabel implements SystemEventListener {
+
+	public static final String COMPONENT_TYPE = "org.omnifaces.component.outputlabel.OutputLabel";
+
+	public static final String COMPONENT_FAMILY = "org.omnifaces.component";
+	
+	public OutputLabel() {
+		if (!isPostback()) {
+			Faces.getViewRoot().subscribeToViewEvent(PreRenderViewEvent.class, this);
+		}
+	}
+	
+	@Override
+	public boolean isListenerForSource(Object source) {
+		return source instanceof UIViewRoot;
+	}
+	
+	@Override
+	public void processEvent(SystemEvent event) throws AbortProcessingException {
+		if (!isPostback()) {
+			String forValue = (String) getAttributes().get("for");
+			if (!isEmpty(forValue)) {
+				UIComponent forComponent = Components.findComponentRelatively(this, forValue);
+				
+				// To be sure, check if the target component doesn't have a label already. This
+				// is unlikely, since otherwise people have no need to use this outputLabel component
+				// but check to be sure.
+				if (Components.getOptionalLabel(forComponent) == null) {
+					ValueExpression valueExpression = getValueExpression("value");
+					if (valueExpression != null) {
+						forComponent.setValueExpression("label", valueExpression);
+					} else {					
+						forComponent.getAttributes().put("label", getValue());
+					}
+				}
+			}
+		}
+		
+	}
+}
--- src/org/omnifaces/util/Components.java
+++ src/org/omnifaces/util/Components.java
@@ -12,8 +12,11 @@
  */
 package org.omnifaces.util;
 
+import static org.omnifaces.util.Utils.isEmpty;
+
 import javax.el.ValueExpression;
 import javax.faces.component.EditableValueHolder;
+import javax.faces.component.NamingContainer;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
@@ -24,6 +27,7 @@ import javax.faces.context.FacesContext;
  * {@link UIComponent}.
  *
  * @author Bauke Scholtz
+ * @author Arjan Tijms
  */
 public final class Components {
 
@@ -55,7 +59,118 @@ public final class Components {
 	public static <T extends UIComponent> T findComponent(String clientId) {
 		return (T) FacesContext.getCurrentInstance().getViewRoot().findComponent(clientId);
 	}
+	
+	
+	/**
+	 * Returns the UI component matching the given client ID search expression relative to the point
+	 * in the component tree of the given component. For this search both parents and children are
+	 * consulted, increasingly moving further away from the given component. Parents are consulted
+	 * first, then children.
+	 * 
+	 * @param component the component from which the relative search is started.
+	 * @param clientId The client ID search expression.
+	 * @return The UI component matching the given client ID search expression.
+	 * @throws ClassCastException When <code>T</code> is of wrong type.
+	 * @see UIComponent#findComponent(String)
+	 */
+	public static <T extends UIComponent> T findComponentRelatively(UIComponent component, String clientId) {
+		
+		if (isEmpty(clientId)) {
+			return null;
+		}
+		
+		// Search first in the naming container parents of the given component
+		T result = findComponentInParents(component, clientId);
+		if (result == null) {
+			// If not in the parents, search from the root
+			result = findComponentInChildren(Faces.getViewRoot(), clientId);
+		}
+		
+		return result;		
+	}
 
+	/**
+	 * Returns the UI component matching the given client ID search expression relative to the point
+	 * in the component tree of the given component, searching only in its parents.
+	 * 
+	 * @param component the component from which the relative search is started.
+	 * @param clientId The client ID search expression.
+	 * @return The UI component matching the given client ID search expression.
+	 * @throws ClassCastException When <code>T</code> is of wrong type.
+	 * @see UIComponent#findComponent(String)
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends UIComponent> T findComponentInParents(UIComponent component, String clientId) {
+		
+		if (isEmpty(clientId)) {
+			return null;
+		}
+
+		UIComponent parent = component;
+		while (parent != null) {
+			
+			UIComponent result = null;
+			if (parent instanceof NamingContainer) {
+				try {
+					result = parent.findComponent(clientId);
+				} catch (IllegalArgumentException e) {
+					continue;
+				}
+			}
+			
+			
+			if (result != null) {
+				return (T) result;
+			}
+			
+			parent = parent.getParent();
+		}
+
+		return null;
+	}
+		
+	/**
+	 * Returns the UI component matching the given client ID search expression relative to the point
+	 * in the component tree of the given component, searching only in its children.
+	 * 
+	 * @param component the component from which the relative search is started.
+	 * @param clientId The client ID search expression.
+	 * @return The UI component matching the given client ID search expression.
+	 * @throws ClassCastException When <code>T</code> is of wrong type.
+	 * @see UIComponent#findComponent(String)
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends UIComponent> T findComponentInChildren(UIComponent component, String clientId) {
+		
+		if (isEmpty(clientId)) {
+			return null;
+		}
+
+		for (UIComponent child : component.getChildren()) {
+
+			UIComponent result = null;
+			if (child instanceof NamingContainer) {
+				try {
+					result = child.findComponent(clientId);
+				} catch (IllegalArgumentException e) {
+					continue;
+				}
+			}
+
+			if (result == null) {
+				result = findComponentInChildren(child, clientId);
+			}
+
+			if (result != null) {
+				return (T) result;
+			}
+		}
+
+		return null;
+	}
+	
+	
+	
 	/**
 	 * Returns from the given component the closest parent of the given parent type, or <code>null</code> if none
 	 * is found.
@@ -88,25 +203,25 @@ public final class Components {
 	}
 
 	/**
-	 * Returns the value of the <code>label</code> attribute associated with the given UI input component if any, else
+	 * Returns the value of the <code>label</code> attribute associated with the given UI component if any, else
 	 * the client ID. It never returns null.
 	 * @param input The UI input component for which the label is to be retrieved.
-	 * @return The value of the <code>label</code> attribute associated with the given UI input component if any, else
+	 * @return The value of the <code>label</code> attribute associated with the given UI component if any, else
 	 * the client ID.
 	 */
-	public static String getLabel(UIInput input) {
+	public static String getLabel(UIComponent input) {
 		Object label = getOptionalLabel(input);
 		return (label != null) ? label.toString() : input.getClientId();
 	}
 	
 	/**
-	 * Returns the value of the <code>label</code> attribute associated with the given UI input component if any, else
+	 * Returns the value of the <code>label</code> attribute associated with the given UI component if any, else
 	 * null.
 	 * @param input The UI input component for which the label is to be retrieved.
-	 * @return The value of the <code>label</code> attribute associated with the given UI input component if any, else
+	 * @return The value of the <code>label</code> attribute associated with the given UI component if any, else
 	 * null.
 	 */
-	public static Object getOptionalLabel(UIInput input) {
+	public static Object getOptionalLabel(UIComponent input) {
 		Object label = input.getAttributes().get("label");
 		if (label == null || (label instanceof String && ((String) label).length() == 0)) {
 			ValueExpression labelExpression = input.getValueExpression("label");
