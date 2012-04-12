d2800e53b79d21a0f65f364a20183648f2a31685
Bauke Scholtz <balusc@gmail.com>
2012-04-12 11:17:55 -0400

    #6: Fix initialization issue in MyFaces where FacesContext apparently isn't available during factory construction

--- src/org/omnifaces/exceptionhandler/FullAjaxExceptionHandler.java
+++ src/org/omnifaces/exceptionhandler/FullAjaxExceptionHandler.java
@@ -12,7 +12,10 @@
  */
 package org.omnifaces.exceptionhandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -24,13 +27,43 @@ import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.PhaseId;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import org.omnifaces.util.Events;
 import org.omnifaces.util.Exceptions;
+import org.omnifaces.util.Faces;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * This exception handler enables you to show the full error page in its entirety to the enduser in case of exceptions
  * during ajax requests. Refer the documentation of {@link FullAjaxExceptionHandlerFactory} how to setup it.
+ * <p>
+ * This exception handler will parse the <tt>web.xml</tt> to find the error page locations of the HTTP error
+ * code <tt>500</tt> and all declared specific exception types. Those locations need to point to Facelets files. The
+ * location of the HTTP error code <tt>500</tt> or the exception type <code>java.lang.Throwable</code> is required in
+ * order to get the full ajax exception handler to work, because there's then at least a fall back error page whenever
+ * there's no match with any of the declared specific exceptions. So, you must at least have either
+ * <pre>
+ * &lt;error-page&gt;
+ *   &lt;error-code&gt;500&lt;/error-code&gt;
+ *   &lt;location&gt;/errors/500.xhtml&lt;/location&gt;
+ * &lt;/error-page&gt;
+ * </pre>
+ * <p>or
+ * <pre>
+ * &lt;error-page&gt;
+ *   &lt;exception-type&gt;java.lang.Throwable&lt;/exception-type&gt;
+ *   &lt;location&gt;/errors/500.xhtml&lt;/location&gt;
+ * &lt;/error-page&gt;
+ * </pre>
+ * <p>
+ * Both can also, only the <code>java.lang.Throwable</code> one will always get precedence over the <tt>500</tt>
+ * one, as per the Servlet API specification, so the <tt>500</tt> one would be basically superfluous.
  * <p>
  * The exception detail is available in the request scope by the standard servlet error request attributes like as in a
  * normal synchronous error page response. You could for example show them in the error page as follows:
@@ -38,22 +71,31 @@ import org.omnifaces.util.Exceptions;
  * &lt;ul&gt;
  *   &lt;li&gt;Date/time: #{of:formatDate(now, 'yyyy-MM-dd HH:mm:ss')}&lt;/li&gt;
  *   &lt;li&gt;User agent: #{header['user-agent']}&lt;/li&gt;
+ *   &lt;li&gt;User IP: #{request.remoteAddr}&lt;/li&gt;
  *   &lt;li&gt;Request URI: #{requestScope['javax.servlet.error.request_uri']}&lt;/li&gt;
+ *   &lt;li&gt;Ajax request: #{facesContext.partialViewContext.ajaxRequest ? 'Yes' : 'No'}&lt;/li&gt;
  *   &lt;li&gt;Status code: #{requestScope['javax.servlet.error.status_code']}&lt;/li&gt;
  *   &lt;li&gt;Exception type: #{requestScope['javax.servlet.error.exception_type']}&lt;/li&gt;
  *   &lt;li&gt;Exception message: #{requestScope['javax.servlet.error.message']}&lt;/li&gt;
- *   &lt;li&gt;Exception stack trace:
+ *   &lt;li&gt;Stack trace:
  *     &lt;pre&gt;#{of:printStackTrace(requestScope['javax.servlet.error.exception'])}&lt;/pre&gt;
  *   &lt;/li&gt;
  * &lt;/ul&gt;
  * </pre>
  *
  * @author Bauke Scholtz
+ * @see FullAjaxExceptionHandlerFactory
  */
 public class FullAjaxExceptionHandler extends ExceptionHandlerWrapper {
 
 	// Private constants ----------------------------------------------------------------------------------------------
 
+	private static final String WEB_XML =
+		"/WEB-INF/web.xml";
+	private static final String ERROR_LOCATION_INVALID =
+		"Error page location '%s' in web.xml is invalid. Resource resolved to null.";
+	private static final String ERROR_DEFAULT_LOCATION_MISSING =
+		"Either HTTP 500 or java.lang.Throwable error page is required in web.xml. Neither was found.";
 	private static final String LOG_EXCEPTION_OCCURRED =
 		"An exception occurred during JSF ajax request. Showing error page location '%s'.";
 
@@ -73,15 +115,11 @@ public class FullAjaxExceptionHandler extends ExceptionHandlerWrapper {
 	// Constructors ---------------------------------------------------------------------------------------------------
 
 	/**
-	 * Construct a new ajax exception handler around the given wrapped exception handler and the given error page
-	 * locations.
+	 * Construct a new ajax exception handler around the given wrapped exception handler.
 	 * @param wrapped The wrapped exception handler.
-	 * @param errorPageLocations Ordered map of all available specific exception types and their locations. The key
-	 * <code>null</code> represents the default location.
 	 */
-	public FullAjaxExceptionHandler(ExceptionHandler wrapped, Map<Class<Throwable>, String> errorPageLocations) {
+	public FullAjaxExceptionHandler(ExceptionHandler wrapped) {
 		this.wrapped = wrapped;
-		this.errorPageLocations = errorPageLocations;
 	}
 
 	// Actions --------------------------------------------------------------------------------------------------------
@@ -179,6 +217,11 @@ public class FullAjaxExceptionHandler extends ExceptionHandlerWrapper {
 	 * @return The right error page location for the given exception.
 	 */
 	private String findErrorPageLocation(Throwable exception) {
+		if (errorPageLocations == null) {
+			// #6: It isn't possible to perform this on webapp's startup which would be more ideal.
+			errorPageLocations = findErrorPageLocations();
+		}
+
 		for (Entry<Class<Throwable>, String> entry : errorPageLocations.entrySet()) {
 			if (entry.getKey() == exception.getClass()) {
 				return entry.getValue();
@@ -192,6 +235,66 @@ public class FullAjaxExceptionHandler extends ExceptionHandlerWrapper {
 		}
 
 		return errorPageLocations.get(null);
+	}
+
+	/**
+	 * Parse <tt>web.xml</tt> and find all error page locations.
+	 * @return An ordered map of all error page locations. The key <code>null</code> represents the default location.
+	 * @throws IllegalArgumentException When an error page location in <tt>web.xml</tt> is missing or invalid.
+	 */
+	@SuppressWarnings("unchecked") // For the cast on Class<Throwable>.
+	private static Map<Class<Throwable>, String> findErrorPageLocations() {
+		Map<Class<Throwable>, String> errorPageLocations = new LinkedHashMap<Class<Throwable>, String>();
+		String defaultLocation = null;
+		InputStream input = null;
+
+		try {
+			input = Faces.getResourceAsStream(WEB_XML);
+
+			if (input != null) { // Since Servlet 3.0, web.xml is optional.
+				Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				defaultLocation =
+					xpath.compile("web-app/error-page[error-code=500]/location").evaluate(document).trim();
+				NodeList exceptionTypes = (NodeList)
+					xpath.compile("web-app/error-page/exception-type").evaluate(document, XPathConstants.NODESET);
+
+				for (int i = 0; i < exceptionTypes.getLength(); i++) {
+					Node node = exceptionTypes.item(i);
+					Class<Throwable> exceptionClass = (Class<Throwable>) Class.forName(node.getTextContent().trim());
+					String exceptionLocation = xpath.compile("location").evaluate(node.getParentNode()).trim();
+
+					if (exceptionClass == Throwable.class) {
+						defaultLocation = exceptionLocation;
+					}
+					else {
+						errorPageLocations.put(exceptionClass, exceptionLocation);
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			// This exception should never occur. If it occurs, then web.xml is broken anyway.
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (input != null) try { input.close(); } catch (IOException ignore) { /**/ }
+		}
+
+		if (defaultLocation == null || defaultLocation.isEmpty()) {
+			throw new IllegalArgumentException(ERROR_DEFAULT_LOCATION_MISSING);
+		}
+		else {
+			errorPageLocations.put(null, defaultLocation);
+		}
+
+		for (String exceptionLocation : errorPageLocations.values()) {
+			if (Faces.getResourceAsStream(exceptionLocation) == null) {
+				throw new IllegalArgumentException(String.format(ERROR_LOCATION_INVALID, exceptionLocation));
+			}
+		}
+
+		return errorPageLocations;
 	}
 
 }
--- src/org/omnifaces/exceptionhandler/FullAjaxExceptionHandlerFactory.java
+++ src/org/omnifaces/exceptionhandler/FullAjaxExceptionHandlerFactory.java
@@ -12,22 +12,8 @@
  */
 package org.omnifaces.exceptionhandler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import javax.faces.context.ExceptionHandler;
 import javax.faces.context.ExceptionHandlerFactory;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
-import org.omnifaces.util.Faces;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  * This exception handler factory needs to be registered as follows in <tt>faces-config.xml</tt> to get the
@@ -39,56 +25,24 @@ import org.w3c.dom.NodeList;
  *   &lt;/exception-handler-factory&gt;
  * &lt;/factory&gt;
  * </pre>
- * <p>This exception handler factory will parse the <tt>web.xml</tt> to find the error page locations of the HTTP error
- * code <tt>500</tt> and all declared specific exception types. Those locations need to point to Facelets files. The
- * location of the HTTP error code <tt>500</tt> or the exception type <code>java.lang.Throwable</code> is required in
- * order to get the full ajax exception handler to work, because there's then at least a fall back error page whenever
- * there's no match with any of the declared specific exceptions. So, you must at least have either
- * <pre>
- * &lt;error-page&gt;
- *   &lt;error-code&gt;500&lt;/error-code&gt;
- *   &lt;location&gt;/errors/500.xhtml&lt;/location&gt;
- * &lt;/error-page&gt;
- * </pre>
- * <p>or
- * <pre>
- * &lt;error-page&gt;
- *   &lt;exception-type&gt;java.lang.Throwable&lt;/exception-type&gt;
- *   &lt;location&gt;/errors/500.xhtml&lt;/location&gt;
- * &lt;/error-page&gt;
- * </pre>
- * <p>Both can also, only the <code>java.lang.Throwable</code> one will always get precedence over the <tt>500</tt>
- * one, as per the Servlet API specification, so the <tt>500</tt> one would be basically superfluous.
  *
  * @author Bauke Scholtz
+ * @see FullAjaxExceptionHandler
  */
 public class FullAjaxExceptionHandlerFactory extends ExceptionHandlerFactory {
-
-	// Constants ------------------------------------------------------------------------------------------------------
-
-	private static final String WEB_XML =
-		"/WEB-INF/web.xml";
-	private static final String ERROR_LOCATION_INVALID =
-		"Error page location '%s' in web.xml is invalid. Resource resolved to null.";
-	private static final String ERROR_DEFAULT_LOCATION_MISSING =
-		"Either HTTP 500 or java.lang.Throwable error page is required in web.xml. Neither was found.";
 
 	// Variables ------------------------------------------------------------------------------------------------------
 
 	private ExceptionHandlerFactory wrapped;
-	private Map<Class<Throwable>, String> errorPageLocations;
 
 	// Constructors ---------------------------------------------------------------------------------------------------
 
 	/**
-	 * Construct a new full ajax exception handler factory around the given wrapped factory. This constructor will
-	 * parse the <tt>web.xml</tt> to find the error page locations.
+	 * Construct a new full ajax exception handler factory around the given wrapped factory.
 	 * @param wrapped The wrapped factory.
-	 * @throws IllegalArgumentException When an error page location in <tt>web.xml</tt> is missing or invalid.
 	 */
 	public FullAjaxExceptionHandlerFactory(ExceptionHandlerFactory wrapped) {
 		this.wrapped = wrapped;
-		this.errorPageLocations = findErrorPageLocations();
 	}
 
 	// Actions --------------------------------------------------------------------------------------------------------
@@ -98,7 +52,7 @@ public class FullAjaxExceptionHandlerFactory extends ExceptionHandlerFactory {
 	 */
 	@Override
 	public ExceptionHandler getExceptionHandler() {
-		return new FullAjaxExceptionHandler(wrapped.getExceptionHandler(), errorPageLocations);
+		return new FullAjaxExceptionHandler(wrapped.getExceptionHandler());
 	}
 
 	/**
@@ -107,68 +61,6 @@ public class FullAjaxExceptionHandlerFactory extends ExceptionHandlerFactory {
 	@Override
 	public ExceptionHandlerFactory getWrapped() {
 		return wrapped;
-	}
-
-	// Helpers --------------------------------------------------------------------------------------------------------
-
-	/**
-	 * Parse <tt>web.xml</tt> and find all error page locations.
-	 * @return An ordered map of all error page locations. The key <code>null</code> represents the default location.
-	 * @throws IllegalArgumentException When an error page location in <tt>web.xml</tt> is missing or invalid.
-	 */
-	@SuppressWarnings("unchecked") // For the cast on Class<Throwable>.
-	private static Map<Class<Throwable>, String> findErrorPageLocations() {
-		Map<Class<Throwable>, String> errorPageLocations = new LinkedHashMap<Class<Throwable>, String>();
-		String defaultLocation = null;
-		InputStream input = null;
-
-		try {
-			input = Faces.getResourceAsStream(WEB_XML);
-
-			if (input != null) { // Since Servlet 3.0, web.xml is optional.
-				Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
-				XPath xpath = XPathFactory.newInstance().newXPath();
-				defaultLocation =
-					xpath.compile("web-app/error-page[error-code=500]/location").evaluate(document).trim();
-				NodeList exceptionTypes = (NodeList)
-					xpath.compile("web-app/error-page/exception-type").evaluate(document, XPathConstants.NODESET);
-
-				for (int i = 0; i < exceptionTypes.getLength(); i++) {
-					Node node = exceptionTypes.item(i);
-					Class<Throwable> exceptionClass = (Class<Throwable>) Class.forName(node.getTextContent().trim());
-					String exceptionLocation = xpath.compile("location").evaluate(node.getParentNode()).trim();
-
-					if (exceptionClass == Throwable.class) {
-						defaultLocation = exceptionLocation;
-					}
-					else {
-						errorPageLocations.put(exceptionClass, exceptionLocation);
-					}
-				}
-			}
-		}
-		catch (Exception e) {
-			// This exception should never occur. If it occurs, then web.xml is broken anyway.
-			throw new RuntimeException(e);
-		}
-		finally {
-			if (input != null) try { input.close(); } catch (IOException ignore) { /**/ }
-		}
-
-		if (defaultLocation == null || defaultLocation.isEmpty()) {
-			throw new IllegalArgumentException(ERROR_DEFAULT_LOCATION_MISSING);
-		}
-		else {
-			errorPageLocations.put(null, defaultLocation);
-		}
-
-		for (String exceptionLocation : errorPageLocations.values()) {
-			if (Faces.getResourceAsStream(exceptionLocation) == null) {
-				throw new IllegalArgumentException(String.format(ERROR_LOCATION_INVALID, exceptionLocation));
-			}
-		}
-
-		return errorPageLocations;
 	}
 
 }
