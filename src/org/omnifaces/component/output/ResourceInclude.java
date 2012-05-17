dd94dd0410db03922d89456a106724ad62774d06
Bauke Scholtz <balusc@gmail.com>
2012-05-17 08:31:21 -0400

    Fixed character encoding bug in ResourceInclude (it was relying on platform default encoding)

--- src/org/omnifaces/component/output/ResourceInclude.java
+++ src/org/omnifaces/component/output/ResourceInclude.java
@@ -12,18 +12,16 @@
  */
 package org.omnifaces.component.output;
 
-import java.io.ByteArrayInputStream;
-import java.io.CharArrayWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Writer;
 
+import javax.faces.FacesException;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponentBase;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -33,93 +31,129 @@ import javax.servlet.http.HttpServletResponseWrapper;
 import org.omnifaces.util.Components;
 
 /**
- * This component is used to catch the output from a Servlet resource and render it as output
+ * This component is used to catch the output from a JSP/Servlet resource and render it as output
  * to the JSF writer. In effect, this allows you to include both Servlets and JSP pages in e.g. Facelets.
  *
  * @author Arjan Tijms
- *
+ * @author Bauke Scholtz
  */
 @FacesComponent(ResourceInclude.COMPONENT_TYPE)
 public class ResourceInclude extends UIComponentBase {
 
+	// Public constants -----------------------------------------------------------------------------------------------
+
+	/** The component type. */
 	public static final String COMPONENT_TYPE = "org.omnifaces.component.output.ResourceInclude";
 
+	/** The standard component family. */
 	public static final String COMPONENT_FAMILY = "org.omnifaces.component.output";
 
+	// UIComponent overrides ------------------------------------------------------------------------------------------
+
+	/**
+	 * Returns {@link #COMPONENT_FAMILY}.
+	 */
 	@Override
 	public String getFamily() {
 		return COMPONENT_FAMILY;
 	}
 
+	/**
+	 * Returns <code>true</code>.
+	 */
+	@Override
+	public boolean getRendersChildren() {
+		return true;
+	}
+
+	/**
+	 * Create a dispatcher for the resource given by the component's path attribute, catch its output and write it to
+	 * the JSF response writer.
+	 */
 	@Override
 	public void encodeBegin(FacesContext context) throws IOException {
 		Components.validateHasNoChildren(this);
 
+		ExternalContext externalContext = context.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+		HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+		BufferedHttpServletResponse bufferedResponse = new BufferedHttpServletResponse(response);
+
 		try {
-			ExternalContext externalContext = context.getExternalContext();
-			HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-			HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
-
-			// Create dispatcher for the resource given by the component's page attribute.
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher((String) getAttributes().get("path"));
-
-			// Catch the resource's output.
-			CharResponseWrapper responseWrapper = new CharResponseWrapper(response);
-			requestDispatcher.include(request, responseWrapper);
-
-			// Write the output from the resource to the JSF response writer.
-			context.getResponseWriter().write(responseWrapper.toString());
+			request.getRequestDispatcher((String) getAttributes().get("path")).include(request, bufferedResponse);
 		}
 		catch (ServletException e) {
-			throw new IOException();
+			throw new FacesException(e);
 		}
+
+		context.getResponseWriter().write(new String(bufferedResponse.getBuffer(), response.getCharacterEncoding()));
 	}
 
-	static class CharResponseWrapper extends HttpServletResponseWrapper {
+	// Nested classes -------------------------------------------------------------------------------------------------
 
-		private CharArrayWriter output;
+	/**
+	 * This HTTP servlet response implementation buffers the entire response body. The buffered response body is as a
+	 * byte array available by {@link #getBuffer()} method. The response writer will use the same character encoding
+	 * as is been set on the response supplied to the constructor. Note that this way any setCharacterEncoding() calls
+	 * on the included JSP/Servlet resourse have thus no effect.
+	 *
+	 * @author Bauke Scholtz
+	 */
+	static class BufferedHttpServletResponse extends HttpServletResponseWrapper {
 
-		@Override
-		public String toString() {
-			return output.toString();
-		}
+	    private final ByteArrayOutputStream buffer;
+	    private PrintWriter writer;
+	    private ServletOutputStream output;
 
-		public CharResponseWrapper(HttpServletResponse response) {
-			super(response);
-			output = new CharArrayWriter();
-		}
+	    public BufferedHttpServletResponse(HttpServletResponse response) {
+	        super(response);
+	        buffer = new ByteArrayOutputStream(response.getBufferSize());
+	    }
 
-		public CharArrayWriter getCharWriter() {
-			return output;
-		}
+	    @Override
+	    public ServletOutputStream getOutputStream() throws IOException {
+	        if (writer != null) {
+	            throw new IllegalStateException("getWriter() has already been called on this response.");
+	        }
 
-		@Override
-		public PrintWriter getWriter() {
-			return new PrintWriter(output);
-		}
+	        if (output == null) {
+	        	output = new ServletOutputStream() {
+					@Override
+					public void write(int b) throws IOException {
+						buffer.write(b);
+					}
+				};
+	        }
 
-		@Override
-		public ServletOutputStream getOutputStream() {
-			return new CharOutputStream(output);
-		}
+	        return output;
+	    }
 
-		public InputStream getInputStream() {
-			return new ByteArrayInputStream(toString().getBytes());
-		}
-	}
+	    @Override
+	    public PrintWriter getWriter() throws IOException {
+	        if (output != null) {
+	            throw new IllegalStateException("getOutputStream() has already been called on this response.");
+	        }
 
-	static class CharOutputStream extends ServletOutputStream {
+	        if (writer == null) {
+	            writer = new PrintWriter(new OutputStreamWriter(buffer, getResponse().getCharacterEncoding()), true);
+	        }
 
-		private Writer output;
+	        return writer;
+	    }
 
-		public CharOutputStream(Writer writer) {
-			output = writer;
-		}
+	    @Override
+	    public void flushBuffer() throws IOException {
+	        if (writer != null) {
+	            writer.flush();
+	        } else if (output != null) {
+	            output.flush();
+	        }
+	    }
 
-		@Override
-		public void write(int b) throws IOException {
-			output.write(b);
-		}
+	    public byte[] getBuffer() throws IOException {
+	    	flushBuffer();
+            return buffer.toByteArray();
+	    }
 
 	}
 
