2c9b309ca0f74d1808482b838a63b83bb35e8dcc
Bauke Scholtz <balusc@gmail.com>
2012-05-29 20:10:34 -0400

    #21: getElementsByName() fallback when ID is not specified

--- src/META-INF/resources/omnifaces/highlight.js
+++ src/META-INF/resources/omnifaces/highlight.js
@@ -5,7 +5,7 @@
  * @see org.omnifaces.component.script.Highlight
  */
 OmniFaces.Highlight = {
-		
+
 	/**
 	 * Add the given error style class to all elements of the given client IDs. If doFocus is <code>true</code>, then
 	 * also set the focus on the first element. All non-existing elements are ignored.
@@ -13,6 +13,14 @@ OmniFaces.Highlight = {
 	addErrorClass: function(clientIds, styleClass, doFocus) {
 		for (var i = 0; i < clientIds.length; i++) {
 			var element = document.getElementById(clientIds[i]);
+
+			if (!element) {
+				var elements = document.getElementsByName(clientIds[i]); // #21
+
+				if (elements && elements.length) {
+					element = elements[0];
+				}
+			}
 
 			if (element) {
 				element.className += ' ' + styleClass;
