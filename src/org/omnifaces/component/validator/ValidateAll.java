8794df6c5e905bbd0d7cc048fac41ba45a3546fa
Bauke Scholtz <balusc@gmail.com>
2012-07-09 12:29:12 -0400

    #40: added o:validateAll

---
+++ src/org/omnifaces/component/validator/ValidateAll.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2012 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.component.validator;
+
+import java.util.List;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Utils;
+
+/**
+ * <strong>ValidateAll</strong> validates if ALL of the given <code>UIInput</code> components have been filled out. The
+ * default message is
+ * <blockquote>{0}: Please fill out all of those fields</blockquote>
+ * <p>
+ * For general usage instructions, refer {@link ValidateMultipleFields} documentation.
+ *
+ * @author Bauke Scholtz
+ */
+@FacesComponent(ValidateAll.COMPONENT_TYPE)
+public class ValidateAll extends ValidateMultipleFields {
+
+	// Public constants -----------------------------------------------------------------------------------------------
+
+	/** The standard component type. */
+	public static final String COMPONENT_TYPE = "org.omnifaces.component.validator.ValidateAll";
+
+	// Private constants ----------------------------------------------------------------------------------------------
+
+	private static final String DEFAULT_MESSAGE = "{0}: Please fill out all of those fields";
+
+	// Constructors ---------------------------------------------------------------------------------------------------
+
+	/**
+	 * The default constructor sets the default message.
+	 */
+	public ValidateAll() {
+		super(DEFAULT_MESSAGE);
+	}
+
+	// Actions --------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Validate if all is filled out.
+	 */
+	@Override
+	protected boolean validateValues(FacesContext context, List<UIInput> inputs, List<Object> values) {
+		for (Object value : values) {
+			if (Utils.isEmpty(value)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+}
