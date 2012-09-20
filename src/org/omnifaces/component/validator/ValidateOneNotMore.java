74174fefac49923fd8c08fe367fffca9f30a3da7
Bauke Scholtz <balusc@gmail.com>
2012-09-20 14:48:39 -0400

    #67: added o:validateOneNotMore

---
+++ src/org/omnifaces/component/validator/ValidateOneNotMore.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2012 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.component.validator;
+
+import java.util.List;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Utils;
+
+/**
+ * <strong>ValidateOneNotMore</strong> validates if only ONE of the given <code>UIInput</code> components has been
+ * filled out. The default message is
+ * <blockquote>{0}: Please fill out only one of those fields</blockquote>
+ * <p>
+ * For general usage instructions, refer {@link ValidateMultipleFields} documentation.
+ *
+ * @author Bauke Scholtz
+ */
+@FacesComponent(ValidateOneNotMore.COMPONENT_TYPE)
+public class ValidateOneNotMore extends ValidateMultipleFields {
+
+	// Public constants -----------------------------------------------------------------------------------------------
+
+	/** The standard component type. */
+	public static final String COMPONENT_TYPE = "org.omnifaces.component.validator.ValidateOneNotMore";
+
+	// Private constants ----------------------------------------------------------------------------------------------
+
+	private static final String DEFAULT_MESSAGE = "{0}: Please fill out only one of those fields";
+
+	// Constructors ---------------------------------------------------------------------------------------------------
+
+	/**
+	 * The default constructor sets the default message.
+	 */
+	public ValidateOneNotMore() {
+		super(DEFAULT_MESSAGE);
+	}
+
+	// Actions --------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Validate if at least one is filled out.
+	 */
+	@Override
+	protected boolean validateValues(FacesContext context, List<UIInput> inputs, List<Object> values) {
+		boolean hasValue = false;
+
+		for (Object value : values) {
+			if (!Utils.isEmpty(value)) {
+				if (hasValue) {
+					return false;
+				}
+
+				hasValue = true;
+			}
+		}
+
+		return hasValue;
+	}
+
+}
