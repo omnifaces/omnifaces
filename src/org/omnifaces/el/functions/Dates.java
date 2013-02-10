947add036a23d2781172f94e2e7764fd1952fc72
Bauke Scholtz <balusc@gmail.com>
2013-02-10 08:04:14 -0400

    fix javadoc error

--- src/org/omnifaces/el/functions/Dates.java
+++ src/org/omnifaces/el/functions/Dates.java
@@ -58,7 +58,7 @@ public final class Dates {
 	 * @param pattern The pattern to format the given date in.
 	 * @return The date which is formatted in the given pattern.
 	 * @throws NullPointerException When the pattern is <code>null</code>.
-	 * @see #formatDateWithTimezone(Date, String, String)
+	 * @see #formatDateWithTimezone(Date, String, Object)
 	 */
 	public static String formatDate(Date date, String pattern) {
 		return formatDate(date, pattern, TimeZone.getDefault());
