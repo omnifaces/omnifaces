b57c62b56b5aa78853c1d70a106d67a3a4f26819
Bauke Scholtz <balusc@gmail.com>
2013-02-19 10:21:32 -0400

    #140: improved WebXml#getSecurityConstraints() and added WebXml#isAccessAllowed(url,role)

--- src/org/omnifaces/config/WebXml.java
+++ src/org/omnifaces/config/WebXml.java
@@ -20,10 +20,12 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -41,9 +43,6 @@ import org.w3c.dom.NodeList;
 /**
  * This configuration enum parses the <tt>/WEB-INF/web.xml</tt> and all <tt>/META-INF/web-fragment</tt> files found in
  * the classpath and offers methods to obtain information from them which is not available by the standard Servlet API.
- * <p>
- * So far there are only methods for obtaining a mapping of all error page locations by exception type and the location
- * of the FORM authentication login page.
  *
  * @author Bauke Scholtz
  * @since 1.2
@@ -68,27 +67,30 @@ public enum WebXml {
 		"error-page/exception-type";
 	private static final String XPATH_LOCATION =
 		"location";
-	private static final String XPATH_500_LOCATION =
+	private static final String XPATH_ERROR_PAGE_500_LOCATION =
 		"error-page[error-code=500]/location";
-	private static final String XPATH_DEFAULT_LOCATION =
+	private static final String XPATH_ERROR_PAGE_DEFAULT_LOCATION =
 		"error-page[not(error-code) and not(exception-type)]/location";
 	private static final String XPATH_FORM_LOGIN_PAGE =
 		"login-config[auth-method='FORM']/form-login-config/form-login-page";
 	private static final String XPATH_SECURITY_CONSTRAINT =
 		"security-constraint";
-	private static final String XPATH_SECURITY_CONSTRAINT_URL_PATTERN =
+	private static final String XPATH_WEB_RESOURCE_URL_PATTERN =
 		"web-resource-collection/url-pattern";
-	private static final String XPATH_SECURITY_CONSTRAINT_AUTH =
+	private static final String XPATH_AUTH_CONSTRAINT =
 		"auth-constraint";
-	private static final String XPATH_SECURITY_CONSTRAINT_AUTH_ROLE =
+	private static final String XPATH_AUTH_CONSTRAINT_ROLE_NAME =
 		"auth-constraint/role-name";
+
+	private static final String URL_MUST_START_WITH_SLASH =
+		"URL must start with '/': '%s'";
 
 	// Properties -----------------------------------------------------------------------------------------------------
 
 	private List<String> welcomeFiles;
 	private Map<Class<Throwable>, String> errorPageLocations;
 	private String formLoginPage;
-	private Map<String, List<String>> securityConstraints;
+	private Map<String, Set<String>> securityConstraints;
 
 	// Constructors ---------------------------------------------------------------------------------------------------
 
@@ -113,7 +115,7 @@ public enum WebXml {
 	// Actions --------------------------------------------------------------------------------------------------------
 
 	/**
-	 * Find for the given exception the right error page location as per Servlet specification 10.9.2:
+	 * Find for the given exception the right error page location as per Servlet 3.0 specification 10.9.2:
 	 * <ul>
 	 *   <li>Make a first pass through all specific exception types. If an exact match is found, use its location.
 	 *   <li>Else make a second pass through all specific exception types in the order as they are declared in
@@ -138,6 +140,78 @@ public enum WebXml {
 		}
 
 		return errorPageLocations.get(null);
+	}
+
+	/**
+	 * Returns <code>true</code> if access to the given URL is allowed for the given role. URL patterns are matched as
+	 * per Servlet 3.0 specification 12.1:
+	 * <ul>
+	 *   <li>Make a first pass through all URL patterns. If an exact match is found, then check the role on it.
+	 *   <li>Else make a recursive pass through all prefix URL patterns, stepping down the URL one directory at a time,
+	 *       trying to find the longest path match. If it is found, then check the role on it.
+	 *   <li>Else if the last segment in the URL path contains an extension, then make a last pass through all suffix
+	 *       URL patterns. If a match is found, then check the role on it.
+	 *   <li>Else return <code>false</code>.
+	 * </ul>
+	 * @param url URL to be checked for access by the given role. It must start with '/' and be context-relative.
+	 * @param role Role to be checked for access to the given URL.
+	 * @return <code>true</code> if access to the given URL is allowed for the given role, otherwise <code>false</code>.
+	 * @throws NullPointerException If given URL is null.
+	 * @throws IllegalArgumentException If given URL does not start with '/'.
+	 */
+	public boolean isAccessAllowed(String url, String role) {
+		if (url.charAt(0) != ('/')) {
+			throw new IllegalArgumentException(String.format(URL_MUST_START_WITH_SLASH, url));
+		}
+
+		if (url.length() > 1 && url.charAt(url.length() - 1) == '/') {
+			url = url.substring(0, url.length() - 1); // Trim leading slash.
+		}
+
+		for (Entry<String, Set<String>> entry : securityConstraints.entrySet()) {
+			if (isExactMatch(entry.getKey(), url)) {
+				return isRoleMatch(entry.getValue(), role);
+			}
+		}
+
+		for (String path = url, urlMatch = ""; !path.isEmpty(); path = path.substring(0, path.lastIndexOf('/'))) {
+			Boolean roleMatch = null;
+
+			for (Entry<String, Set<String>> entry : securityConstraints.entrySet()) {
+				if (urlMatch.length() < entry.getKey().length() && isPrefixMatch(entry.getKey(), path)) {
+					urlMatch = entry.getKey();
+					roleMatch = isRoleMatch(entry.getValue(), role);
+				}
+			}
+
+			if (roleMatch != null) {
+				return roleMatch;
+			}
+		}
+
+		for (Entry<String, Set<String>> entry : securityConstraints.entrySet()) {
+			if (isSuffixMatch(url, entry.getKey())) {
+				return isRoleMatch(entry.getValue(), role);
+			}
+		}
+
+		return false;
+	}
+
+	private static boolean isExactMatch(String urlPattern, String url) {
+		return url.equals(urlPattern.endsWith("/*") ? urlPattern.substring(0, urlPattern.length() - 2) : urlPattern);
+	}
+
+	private static boolean isPrefixMatch(String urlPattern, String url) {
+		return urlPattern.endsWith("/*") ? url.startsWith(urlPattern.substring(0, urlPattern.length() - 2)) : false;
+	}
+
+	private static boolean isSuffixMatch(String urlPattern, String url) {
+		return urlPattern.startsWith("*.") ? url.endsWith(urlPattern.substring(1)) : false;
+	}
+
+	private static boolean isRoleMatch(Set<String> roles, String role) {
+		return roles == null || roles.contains(role) || (role != null && roles.contains("*"));
 	}
 
 	// Getters --------------------------------------------------------------------------------------------------------
@@ -171,11 +245,12 @@ public enum WebXml {
 	/**
 	 * Returns a mapping of all security constraint URL patterns and the associated roles in the declared order. If the
 	 * roles is <code>null</code>, then it means that no auth constraint is been set (i.e. the resource is publicly
-	 * accessible and overrides all previously declared auth constraints on the matching URL pattern). If the roles is
-	 * empty, then it means that an empty auth constraint is been set (i.e. the resource is in no way accessible).
+	 * accessible). If the roles is empty, then it means that an empty auth constraint is been set (i.e. the resource
+	 * is in no way accessible).
 	 * @return A mapping of all security constraint URL patterns and the associated roles in the declared order.
+	 * @since 1.4
 	 */
-	public Map<String, List<String>> getSecurityConstraints() {
+	public Map<String, Set<String>> getSecurityConstraints() {
 		return securityConstraints;
 	}
 
@@ -274,10 +349,10 @@ public enum WebXml {
 		}
 
 		if (!errorPageLocations.containsKey(null)) {
-			String defaultLocation = xpath.compile(XPATH_500_LOCATION).evaluate(webXml).trim();
+			String defaultLocation = xpath.compile(XPATH_ERROR_PAGE_500_LOCATION).evaluate(webXml).trim();
 
 			if (isEmpty(defaultLocation)) {
-				defaultLocation = xpath.compile(XPATH_DEFAULT_LOCATION).evaluate(webXml).trim();
+				defaultLocation = xpath.compile(XPATH_ERROR_PAGE_DEFAULT_LOCATION).evaluate(webXml).trim();
 			}
 
 			if (!isEmpty(defaultLocation)) {
@@ -299,27 +374,27 @@ public enum WebXml {
 	/**
 	 * Create and return a mapping of all security constraint URL patterns and the associated roles.
 	 */
-	private static Map<String, List<String>> parseSecurityConstraints(Element webXml, XPath xpath) throws Exception {
-		Map<String, List<String>> securityConstraints = new LinkedHashMap<String, List<String>>();
+	private static Map<String, Set<String>> parseSecurityConstraints(Element webXml, XPath xpath) throws Exception {
+		Map<String, Set<String>> securityConstraints = new LinkedHashMap<String, Set<String>>();
 		NodeList constraints = getNodeList(webXml, xpath, XPATH_SECURITY_CONSTRAINT);
 
 		for (int i = 0; i < constraints.getLength(); i++) {
 			Node constraint = constraints.item(i);
-			List<String> roles = null;
-			NodeList auth = getNodeList(constraint, xpath, XPATH_SECURITY_CONSTRAINT_AUTH);
+			Set<String> roles = null;
+			NodeList auth = getNodeList(constraint, xpath, XPATH_AUTH_CONSTRAINT);
 
 			if (auth.getLength() > 0) {
-				NodeList authRoles = getNodeList(constraint, xpath, XPATH_SECURITY_CONSTRAINT_AUTH_ROLE);
-				roles = new ArrayList<String>(authRoles.getLength());
+				NodeList authRoles = getNodeList(constraint, xpath, XPATH_AUTH_CONSTRAINT_ROLE_NAME);
+				roles = new HashSet<String>(authRoles.getLength());
 
 				for (int j = 0; j < authRoles.getLength(); j++) {
 					roles.add(authRoles.item(j).getTextContent().trim());
 				}
 
-				roles = Collections.unmodifiableList(roles);
+				roles = Collections.unmodifiableSet(roles);
 			}
 
-			NodeList urlPatterns = getNodeList(constraint, xpath, XPATH_SECURITY_CONSTRAINT_URL_PATTERN);
+			NodeList urlPatterns = getNodeList(constraint, xpath, XPATH_WEB_RESOURCE_URL_PATTERN);
 
 			for (int j = 0; j < urlPatterns.getLength(); j++) {
 				String urlPattern = urlPatterns.item(j).getTextContent().trim();
