47d0e611e70d79caf65f9031741cacfe499d2391
Arjan Tijms <arjantijms@gmail.com>
2013-02-27 00:50:13 +0100

    Added and fixed comments.

--- src/org/omnifaces/facesviews/ExtensionAction.java
+++ src/org/omnifaces/facesviews/ExtensionAction.java
@@ -1,5 +1,28 @@
+/*
+ * Copyright 2013 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.omnifaces.facesviews;
 
+/**
+ * The action that is done when a Faces Views request with an extension is done.
+ * <p>
+ * Note that this is only used for views that were discovered via Faces Views. It has
+ * no affect on other resources, even if they have the same extension.
+ * 
+ * @author Arjan Tijms
+ * @since 1.4
+ */
 public enum ExtensionAction {
-	SEND_404, REDIRECT_TO_EXTENSIONLESS, PROCEED
+	SEND_404, // Send a 404 (not found), makes it look like e.g. "foo.xhtml" never existed and there's only "foo"
+	REDIRECT_TO_EXTENSIONLESS, // Redirects to the same URL, but with the extension removed. E.g. /foo.xhtml is redirected to /foo
+	PROCEED // No special action is taken. Both /foo.xhtml and /foo are processed as-if they were separate views (with same content)
 }
