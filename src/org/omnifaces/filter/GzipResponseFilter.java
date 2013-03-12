1e7688bb2b5407e9e2f23047da450c57ead10ff4
Bauke Scholtz <balusc@gmail.com>
2013-03-12 07:58:56 -0400

    #152: removed block which was introduced during FacesViews testing/fixing

--- src/org/omnifaces/filter/GzipResponseFilter.java
+++ src/org/omnifaces/filter/GzipResponseFilter.java
@@ -146,10 +146,7 @@ public class GzipResponseFilter extends HttpFilter {
 		if (acceptsGzip(request)) {
 			GzipHttpServletResponse gzipResponse = new GzipHttpServletResponse(response, threshold, mimetypes);
 			chain.doFilter(request, gzipResponse);
-
-			if (!gzipResponse.isCommitted()) { // Some other filter may have performed a forward/redirect.
-				gzipResponse.close(); // Mandatory for the case the threshold limit hasn't been reached.
-			}
+			gzipResponse.close(); // Mandatory for the case the threshold limit hasn't been reached.
 		}
 		else {
 			chain.doFilter(request, response);
