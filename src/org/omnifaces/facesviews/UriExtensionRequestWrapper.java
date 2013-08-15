0d9a716f6c924bd61f5a2668c317110ec12f647b
Bauke Scholtz <balusc@gmail.com>
2013-08-15 15:37:52 -0400

    Something went wrong in <o:form useRequestURI> when used with FacesViews. Removed getRequestURI/URL overrides, just to test (should theoretically break nothing; this commit might be reverted later if necessary)

--- src/org/omnifaces/facesviews/UriExtensionRequestWrapper.java
+++ src/org/omnifaces/facesviews/UriExtensionRequestWrapper.java
@@ -23,58 +23,35 @@ import javax.servlet.http.HttpServletRequestWrapper;
  * prefix (path) or suffix (extension) mapping was used. If the request is neither
  * (in effect, an "exact and extensionless mapping), JSF will get confused and not
  * be able to derive view IDs etc correctly.
- * 
+ *
  * @author Arjan Tijms
  * @since 1.4
  */
 public class UriExtensionRequestWrapper extends HttpServletRequestWrapper {
-	
+
 	private final String extension;
 
 	public UriExtensionRequestWrapper(HttpServletRequest request, String extension) {
 		super(request);
 		this.extension = extension;
 	}
-	
+
 	@Override
 	public String getServletPath() {
-		
+
 		String servletPath = super.getServletPath();
 		if (servletPath.endsWith(extension)) {
 			return servletPath;
 		}
-		
+
 		return servletPath + extension;
 	}
-	
-	@Override
-	public String getRequestURI() {
-		
-		String requestURI = super.getRequestURI();
-		if (requestURI.endsWith(extension)) {
-			return requestURI;
-		}
-		
-		return requestURI + extension;
-	}
-	
-	@Override
-	public StringBuffer getRequestURL() {
-		
-		StringBuffer requestURL = super.getRequestURL();
-		int extensionPos = requestURL.lastIndexOf(extension);
-		if (extensionPos != -1 && requestURL.length() - extension.length() == extensionPos) {
-			return requestURL;
-		}
-		
-		return requestURL.append(extension);
-	}
-	
+
 	@Override
 	public String getPathInfo() {
 		// Since we simulate that the request mapped to an extension and not to a prefix path, there
 		// can be no path info.
 		return null;
 	}
-	
+
 }
