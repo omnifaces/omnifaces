04ec63ee63fbad360f871feac823e594dc6bbc2e
Bauke Scholtz <balusc@gmail.com>
2013-11-26 14:50:02 -0400

    #269: new <o:validateMultiple> tag

---
+++ src/org/omnifaces/component/validator/ValidateMultiple.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2013 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.component.validator;
+
+import java.util.List;
+
+import javax.el.ELContext;
+import javax.el.MethodExpression;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+
+import org.omnifaces.validator.MultiFieldValidator;
+
+/**
+ * <strong>ValidateMultiple</strong> allows the developer to validate multiple fields by a custom validator method:
+ * <pre>
+ * &lt;o:validateMultiple id="myId" components="foo bar baz" validator="#{bean.validateValues}" /&gt;
+ * &lt;h:message for="myId" /&gt;
+ * &lt;h:inputText id="foo" /&gt;
+ * &lt;h:inputText id="bar" /&gt;
+ * &lt;h:inputText id="baz" /&gt;
+ * </pre>
+ * <pre>
+ * public boolean validateValues(FacesContext context, List&lt;UIInput&gt; components, List&lt;Object&gt; values) {
+ *     // ...
+ * }
+ * </pre>
+ * <p>Or by a managed bean instance which implements the {@link MultiFieldValidator} interface:
+ * <pre>
+ * &lt;o:validateMultiple id="myId" components="foo bar baz" validator="#{validateValuesBean}" /&gt;
+ * &lt;h:message for="myId" /&gt;
+ * &lt;h:inputText id="foo" /&gt;
+ * &lt;h:inputText id="bar" /&gt;
+ * &lt;h:inputText id="baz" /&gt;
+ * </pre>
+ * <pre>
+ * {@literal @}ManagedBean
+ * {@literal @}RequestScoped
+ * public class ValidateValuesBean implements MultiFieldValidator {
+ *     {@literal @}Override
+ *     boolean validateValues(FacesContext context, List&lt;UIInput&gt; components, List&lt;Object&gt; values) {
+ *         // ...
+ *     }
+ * }
+ * </pre>
+ *
+ * @author Juliano Marques
+ * @author Baukt Scholtz
+ * @since 1.7
+ */
+@FacesComponent(ValidateMultiple.COMPONENT_TYPE)
+public class ValidateMultiple extends ValidateMultipleFields {
+
+	// Public constants -----------------------------------------------------------------------------------------------
+
+	/** The standard component type. */
+	public static final String COMPONENT_TYPE = "org.omnifaces.component.validator.ValidateMultiple";
+
+	// Private constants ----------------------------------------------------------------------------------------------
+
+	private static final String DEFAULT_MESSAGE = "{0}: Invalid value";
+
+	private enum PropertyKeys {
+		validateMethod
+	}
+
+	// Vars -----------------------------------------------------------------------------------------------------------
+
+	private MultiFieldValidator validator;
+
+	// Constructors ---------------------------------------------------------------------------------------------------
+
+	/**
+	 * The default constructor sets the default message.
+	 */
+	public ValidateMultiple() {
+		super(DEFAULT_MESSAGE);
+	}
+
+	// Actions --------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Invoke the validator and return its outcome.
+	 */
+	@Override
+	public boolean validateValues(FacesContext context, List<UIInput> components, List<Object> values) {
+		if (validator != null) {
+			return validator.validateValues(context, components, values);
+		}
+		else {
+			ELContext elContext = context.getELContext();
+			return (Boolean) getValidateMethod().invoke(elContext, new Object[] { context, components, values });
+		}
+	}
+
+	// Getters/setters ------------------------------------------------------------------------------------------------
+
+	/**
+	 * Returns the validator instance.
+	 * @return The validator instance.
+	 */
+	public MultiFieldValidator getValidator() {
+		return validator;
+	}
+
+	/**
+	 * Sets the validator instance.
+	 * @param validator The validator instance.
+	 */
+	public void setValidator(MultiFieldValidator validator) {
+		this.validator = validator;
+	}
+
+	/**
+	 * Returns the validator method expression.
+	 * @return The validator method expression.
+	 */
+	public MethodExpression getValidateMethod() {
+		return (MethodExpression) getStateHelper().eval(PropertyKeys.validateMethod);
+	}
+
+	/**
+	 * Sets the validator method expression.
+	 * @param validator The validator method expression.
+	 */
+	public void setValidateMethod(MethodExpression validateMethod) {
+		getStateHelper().put(PropertyKeys.validateMethod, validateMethod);
+	}
+
+}
---
+++ src/org/omnifaces/component/validator/ValidateMultipleHandler.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2013 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.component.validator;
+
+import java.util.List;
+
+import javax.el.MethodExpression;
+import javax.el.ValueExpression;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.ComponentConfig;
+import javax.faces.view.facelets.ComponentHandler;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagAttribute;
+
+import org.omnifaces.validator.MultiFieldValidator;
+
+/**
+ * A handler for {@link ValidateMultiple} component, which will take care of setting the <code>validator</code>
+ * attribute the right way as either {@link ValueExpression} or {@link MethodExpression}.
+ *
+ * @author Juliano Marques
+ * @author Bauke Scholtz
+ * @since 1.7
+ */
+public class ValidateMultipleHandler extends ComponentHandler {
+
+	// Constants ------------------------------------------------------------------------------------------------------
+
+	private static final Class<?>[] PARAM_TYPES = { FacesContext.class, List.class, List.class };
+
+	// Constructors ---------------------------------------------------------------------------------------------------
+
+	/**
+	 * Construct the tag handler for {@link ValidateMultiple} component.
+	 * @param config The component config.
+	 */
+	public ValidateMultipleHandler(ComponentConfig config) {
+		super(config);
+	}
+
+	// Actions --------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Delegate to super and then try to get a {@link ValueExpression} representing a {@link MultiFieldValidator}
+	 * implementation and set it as <code>validator</code> property of current {@link ValidateMultiple} component, or
+	 * if it couldn't be obtained, then get a {@link MethodExpression} representing a method with the same signature
+	 * as {@link MultiFieldValidator#validateValues(FacesContext, List, List)} and set it as <code>validateMethod</code>
+	 * property of current {@link ValidateMultiple} component.
+	 */
+	@Override
+	public void setAttributes(FaceletContext context, Object component) {
+		super.setAttributes(context, component);
+		ValidateMultiple validateMultiple = (ValidateMultiple) component;
+		TagAttribute attribute = getRequiredAttribute("validator");
+
+		try { // Yes, this try-catch is ugly, but there's really no clean way to distinguish a VE from a ME.
+			ValueExpression valueExpression = attribute.getValueExpression(context, MultiFieldValidator.class);
+			MultiFieldValidator validator = (MultiFieldValidator) valueExpression.getValue(context);
+			validateMultiple.setValidator(validator);
+		}
+		catch (Exception e) { // At least, ClassCastException and PropertyNotFoundException are expected.
+			MethodExpression validateMethod = attribute.getMethodExpression(context, boolean.class, PARAM_TYPES);
+			validateMultiple.setValidateMethod(validateMethod);
+		}
+	}
+
+}
