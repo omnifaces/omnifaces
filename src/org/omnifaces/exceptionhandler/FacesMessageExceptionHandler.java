0ff238256a44163fc3301fe058f3316bf77c1796
Bauke Scholtz <bauke.scholtz@kizitos.com>
2014-04-22 15:07:25 +0200

    Fixes #16: added FacesMessageExceptionHandler

---
+++ src/org/omnifaces/exceptionhandler/FacesMessageExceptionHandler.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2014 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.exceptionhandler;
+
+import static org.omnifaces.util.Messages.addGlobal;
+
+import java.util.Iterator;
+
+import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerWrapper;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ExceptionQueuedEvent;
+
+/**
+ * This exception handler will add every exception as a global FATAL faces message. Refer to the documentation of
+ * {@link FacesMessageExceptionHandlerFactory} to see how to set this up.
+ * <p>
+ * <strong>Note:</strong> it's your own responsibility to make sure that the faces messages are being shown. Make sure
+ * that there's a <code>&lt;h:messages&gt;</code> or any equivalent component (OmniFaces, PrimeFaces, etc) is present
+ * in the view and that it can handle global messages and that it's explicitly or automatically updated in case of ajax
+ * requests.
+ * <p>
+ * If more fine grained control of creating the FATAL faces message is desired, then the developer can opt to extend
+ * this {@link FacesMessageExceptionHandler} and override the following method:
+ * <ul>
+ * <li>{@link #createFatalMessage(Throwable)}
+ * </ul>
+ *
+ * @author Bauke Scholtz
+ * @see FacesMessageExceptionHandlerFactory
+ */
+public class FacesMessageExceptionHandler extends ExceptionHandlerWrapper {
+
+	// Variables ------------------------------------------------------------------------------------------------------
+
+	private ExceptionHandler wrapped;
+
+	// Constructors ---------------------------------------------------------------------------------------------------
+
+	/**
+	 * Construct a new faces message exception handler around the given wrapped exception handler.
+	 * @param wrapped The wrapped exception handler.
+	 */
+	public FacesMessageExceptionHandler(ExceptionHandler wrapped) {
+		this.wrapped = wrapped;
+	}
+
+	// Actions --------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Set every exception as a global FATAL faces message.
+	 */
+	@Override
+	public void handle() throws FacesException {
+		for (Iterator<ExceptionQueuedEvent> iter = getUnhandledExceptionQueuedEvents().iterator(); iter.hasNext();) {
+			String message = createFatalMessage(iter.next().getContext().getException());
+			addGlobal(new FacesMessage(FacesMessage.SEVERITY_FATAL, message, null));
+			iter.remove();
+		}
+
+		wrapped.handle();
+	}
+
+	/**
+	 * Create fatal message based on given exception which will in turn be passed to
+	 * {@link FacesContext#addMessage(String, javax.faces.application.FacesMessage)}.
+	 * The default implementation returns {@link Throwable#toString()}.
+	 * @param exception The exception to create fatal message for.
+	 * @return The fatal message created based on the given exception.
+	 */
+	protected String createFatalMessage(Throwable exception) {
+		return exception.toString();
+	}
+
+	@Override
+	public ExceptionHandler getWrapped() {
+		return wrapped;
+	}
+
+}
---
+++ src/org/omnifaces/exceptionhandler/FacesMessageExceptionHandlerFactory.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.exceptionhandler;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+/**
+ * This exception handler factory needs to be registered as follows in <code>faces-config.xml</code> to get the
+ * {@link FacesMessageExceptionHandler} to run:
+ * <pre>
+ * &lt;factory&gt;
+ *   &lt;exception-handler-factory&gt;
+ *     org.omnifaces.exceptionhandler.FacesMessageExceptionHandlerFactory
+ *   &lt;/exception-handler-factory&gt;
+ * &lt;/factory&gt;
+ * </pre>
+ *
+ * @author Bauke Scholtz
+ * @see FacesMessageExceptionHandler
+ */
+public class FacesMessageExceptionHandlerFactory extends ExceptionHandlerFactory {
+
+	// Variables ------------------------------------------------------------------------------------------------------
+
+	private ExceptionHandlerFactory wrapped;
+
+	// Constructors ---------------------------------------------------------------------------------------------------
+
+	/**
+	 * Construct a new full ajax exception handler factory around the given wrapped factory.
+	 * @param wrapped The wrapped factory.
+	 */
+	public FacesMessageExceptionHandlerFactory(ExceptionHandlerFactory wrapped) {
+		this.wrapped = wrapped;
+	}
+
+	// Actions --------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Returns a new instance of {@link FacesMessageExceptionHandler} which wraps the original exception handler.
+	 */
+	@Override
+	public ExceptionHandler getExceptionHandler() {
+		return new FacesMessageExceptionHandler(wrapped.getExceptionHandler());
+	}
+
+	/**
+	 * Returns the wrapped factory.
+	 */
+	@Override
+	public ExceptionHandlerFactory getWrapped() {
+		return wrapped;
+	}
+
+}
