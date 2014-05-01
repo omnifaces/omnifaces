de07be5cf99ae5dbffb155c67ff045b669e014ac
Bauke Scholtz <bauke.scholtz@kizitos.com>
2014-05-01 14:40:33 +0200

    #10 improvement: add target attribute anyway

--- src/org/omnifaces/taghandler/MassAttribute.java
+++ src/org/omnifaces/taghandler/MassAttribute.java
@@ -42,8 +42,8 @@ import javax.faces.view.facelets.TagHandler;
  *     &lt;h:inputText id="input5" disabled="#{false}" /&gt;
  * &lt;/o:massAttribute&gt;
  * </pre>
- * will only set the <code>disabled="true"</code> attribute in <code>input1</code>, <code>input2</code> and
- * <code>input3</code>.
+ * will set the <code>disabled="true"</code> attribute in <code>input1</code>, <code>input2</code> and
+ * <code>input3</code> as those are the only components <strong>without</strong> a value expression on the boolean attribute.
  * <p>
  * As another general example without booleans, the following setup
  * <pre>
@@ -57,6 +57,19 @@ import javax.faces.view.facelets.TagHandler;
  * will only set the <code>styleClass="#{component.valid ? '' : 'error'}"</code> attribute in <code>input1</code> as
  * that's the only component on which the attribute is absent.
  * Do note that the specified EL expression will actually be evaluated on a per-component basis.
+ * <p>
+ * To target a specific component (super)class, use the <code>target</code> attribute. The below example skips labels
+ * (as that would in the below example otherwise fail because they don't have a <code>valid</code> property):
+ * <pre>
+ * &lt;o:massAttribute name="styleClass" value="#{component.valid ? '' : 'error'}" target="javax.faces.component.UIInput"&gt;
+ *     &lt;h:outputLabel for="input1" /&gt;
+ *     &lt;h:inputText id="input1" /&gt;
+ *     &lt;h:outputLabel for="input2" /&gt;
+ *     &lt;h:inputText id="input2" /&gt;
+ *     &lt;h:outputLabel for="input3" /&gt;
+ *     &lt;h:inputText id="input3" /&gt;
+ * &lt;/o:massAttribute&gt;
+ * </pre>
  *
  * @author Bauke Scholtz
  * @since 1.8
@@ -67,11 +80,16 @@ public class MassAttribute extends TagHandler {
 
 	private static final Set<String> ILLEGAL_NAMES = unmodifiableSet("id", "binding");
 	private static final String ERROR_ILLEGAL_NAME = "The 'name' attribute may not be set to 'id' or 'binding'.";
+	private static final String ERROR_UNAVAILABLE_TARGET = "The 'target' attribute must represent a valid class name."
+		+ " Encountered '%s' which cannot be found in the classpath.";
+	private static final String ERROR_INVALID_TARGET = "The 'target' attribute must represent an UIComponent class."
+		+ " Encountered '%s' which is not an UIComponent class.";
 
 	// Properties -----------------------------------------------------------------------------------------------------
 
 	private String name;
 	private TagAttribute value;
+	private Class<UIComponent> targetClass;
 
 	// Constructors ---------------------------------------------------------------------------------------------------
 
@@ -79,6 +97,7 @@ public class MassAttribute extends TagHandler {
 	 * The tag constructor.
 	 * @param config The tag config.
 	 */
+	@SuppressWarnings("unchecked")
 	public MassAttribute(TagConfig config) {
 		super(config);
 		name = getRequiredAttribute("name").getValue();
@@ -88,6 +107,25 @@ public class MassAttribute extends TagHandler {
 		}
 
 		value = getRequiredAttribute("value");
+		TagAttribute target = getAttribute("target");
+
+		if (target != null) {
+			String className = target.getValue();
+			Class<?> cls = null;
+
+			try {
+				cls = Class.forName(className);
+			}
+			catch (ClassNotFoundException e) {
+				throw new IllegalArgumentException(String.format(ERROR_UNAVAILABLE_TARGET, className));
+			}
+
+			if (!UIComponent.class.isAssignableFrom(cls)) {
+				throw new IllegalArgumentException(String.format(ERROR_INVALID_TARGET, cls));
+			}
+
+			targetClass = (Class<UIComponent>) cls;
+		}
 	}
 
 	// Actions --------------------------------------------------------------------------------------------------------
@@ -103,12 +141,14 @@ public class MassAttribute extends TagHandler {
 
 	private void applyMassAttribute(FaceletContext context, List<UIComponent> children) {
 		for (UIComponent component : children) {
-			if (component.getValueExpression(name) == null) {
-				Object literalValue = component.getAttributes().get(name);
+			if (targetClass == null || targetClass.isAssignableFrom(component.getClass())) {
+				if (component.getValueExpression(name) == null) {
+					Object literalValue = component.getAttributes().get(name);
 
-				if (literalValue == null || literalValue instanceof Boolean) {
-					Class<?> type = (literalValue == null) ? Object.class : Boolean.class;
-					component.setValueExpression(name, value.getValueExpression(context, type));
+					if (literalValue == null || literalValue instanceof Boolean) {
+						Class<?> type = (literalValue == null) ? Object.class : Boolean.class;
+						component.setValueExpression(name, value.getValueExpression(context, type));
+					}
 				}
 			}
 
