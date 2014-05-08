b036e0085245634a81ae486ec76a5bf8457b7bce
arjan tijms <arjan.tijms@gmail.com>
2014-05-09 00:51:15 +0200

    Added utility method to test if any argument is empty and made javadoc a bit more consistent.

--- src/org/omnifaces/util/Utils.java
+++ src/org/omnifaces/util/Utils.java
@@ -78,50 +78,50 @@ public final class Utils {
 	// Lang -----------------------------------------------------------------------------------------------------------
 
 	/**
-	 * Returns true if the given string is null or is empty.
+	 * Returns <code>true</code> if the given string is null or is empty.
 	 *
 	 * @param string The string to be checked on emptiness.
-	 * @return True if the given string is null or is empty.
+	 * @return <code>true</code> if the given string is null or is empty.
 	 */
 	public static boolean isEmpty(String string) {
 		return string == null || string.isEmpty();
 	}
 
 	/**
-	 * Returns true if the given array is null or is empty.
+	 * Returns <code>true</code> if the given array is null or is empty.
 	 *
 	 * @param array The array to be checked on emptiness.
-	 * @return True if the given array is null or is empty.
+	 * @return <code>true</code> if the given array is null or is empty.
 	 */
 	public static boolean isEmpty(Object[] array) {
 		return array == null || array.length == 0;
 	}
 
 	/**
-	 * Returns true if the given collection is null or is empty.
+	 * Returns <code>true</code> if the given collection is null or is empty.
 	 *
 	 * @param collection The collection to be checked on emptiness.
-	 * @return True if the given collection is null or is empty.
+	 * @return <code>true</code> if the given collection is null or is empty.
 	 */
 	public static boolean isEmpty(Collection<?> collection) {
 		return collection == null || collection.isEmpty();
 	}
 
 	/**
-	 * Returns true if the given map is null or is empty.
+	 * Returns <code>true</code> if the given map is null or is empty.
 	 *
 	 * @param map The map to be checked on emptiness.
-	 * @return True if the given map is null or is empty.
+	 * @return <code>true</code> if the given map is null or is empty.
 	 */
 	public static boolean isEmpty(Map<?, ?> map) {
 		return map == null || map.isEmpty();
 	}
 
 	/**
-	 * Returns true if the given value is null or is empty. Types of String, Collection, Map and Array are
+	 * Returns <code>true</code> if the given value is null or is empty. Types of String, Collection, Map and Array are
 	 * recognized. If none is recognized, then examine the emptiness of the toString() representation instead.
 	 * @param value The value to be checked on emptiness.
-	 * @return True if the given value is null or is empty.
+	 * @return <code>true</code> if the given value is null or is empty.
 	 */
 	public static boolean isEmpty(Object value) {
 		if (value == null) {
@@ -143,9 +143,26 @@ public final class Utils {
 		    return value.toString() == null || value.toString().isEmpty();
 		}
 	}
+	
+	/**
+	 * Returns <code>true</code> if at least one value is empty.
+	 *
+	 * @param values the values to be checked on emptiness
+	 * @return <code>true</code> if any value is empty and <code>false</code> if no values are empty
+	 * @since 1.8
+	 */
+	public static boolean isAnyEmpty(Object... values) {
+		for (Object value : values) {
+			if (isEmpty(value)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 
 	/**
-	 * Returns true if the given string is null or is empty or contains whitespace only. In addition to
+	 * Returns <code>true</code> if the given string is null or is empty or contains whitespace only. In addition to
 	 * {@link #isEmpty(String)}, this thus also returns <code>true</code> when <code>string.trim().isEmpty()</code>
 	 * returns <code>true</code>.
 	 *
@@ -158,10 +175,10 @@ public final class Utils {
 	}
 
 	/**
-	 * Returns true if the given string is parseable as a number. I.e. it is not null, nor blank and contains solely
+	 * Returns <code>true</code> if the given string is parseable as a number. I.e. it is not null, nor blank and contains solely
 	 * digits. I.e., it won't throw a <code>NumberFormatException</code> when parsing as <code>Long</code>.
 	 * @param string The string to be checked as number.
-	 * @return True if the given string is parseable as a number.
+	 * @return <code>true</code> if the given string is parseable as a number.
 	 * @since 1.5.
 	 */
 	public static boolean isNumber(String string) {
@@ -176,10 +193,10 @@ public final class Utils {
 	}
 
 	/**
-	 * Returns true if the given string is parseable as a decimal. I.e. it is not null, nor blank and contains solely
+	 * Returns <code>true</code> if the given string is parseable as a decimal. I.e. it is not null, nor blank and contains solely
 	 * digits. I.e., it won't throw a <code>NumberFormatException</code> when parsing as <code>Double</code>.
 	 * @param string The string to be checked as decimal.
-	 * @return True if the given string is parseable as a decimal.
+	 * @return <code>true</code> if the given string is parseable as a decimal.
 	 * @since 1.5.
 	 */
 	public static boolean isDecimal(String string) {
