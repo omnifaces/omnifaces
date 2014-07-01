dc241571b2af58cd22b20495404d7cc58774fb9b
arjan.tijms <arjan.tijms@kizitos.com>
2014-07-01 17:42:35 +0200

    Moved instanstiating eager application scoped beans from CDI extension to ServletContextListener. The latter seems to be a safer place, as beans instantiated from within the extension are prone to hang when certain types of injections are done in it.

--- src/main/java/org/omnifaces/cdi/eager/EagerExtension.java
+++ src/main/java/org/omnifaces/cdi/eager/EagerExtension.java
@@ -112,7 +112,6 @@ public class EagerExtension implements Extension {
 
 		if (!applicationScopedBeans.isEmpty()) {
 			eagerBeansRepository.setApplicationScopedBeans(unmodifiableList(applicationScopedBeans));
-			eagerBeansRepository.instantiateApplicationScoped();
 		}
 
 		if (!sessionScopedBeans.isEmpty()) {
