32113c7c9d0e022cd7ea8c886c5867ab824d4591
arjan.tijms <arjan.tijms@kizitos.com>
2014-07-04 15:43:27 +0200

    Handler for providing (Facelets) component extra services.

---
+++ src/main/java/org/omnifaces/component/util/FaceletContextConsumer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.component.util;
+
+import javax.faces.view.facelets.FaceletContext;
+
+import org.omnifaces.taghandler.ComponentExtraHandler;
+
+/**
+ * Interface to be implemented by components that wish to receive the {@link FaceletContext} for the
+ * Facelet in which they are declared.
+ * 
+ * <p>
+ * This has to be combined with the {@link ComponentExtraHandler}.
+ * 
+ * @since 2.0
+ * @author Arjan Tijms
+ * 
+ */
+public interface FaceletContextConsumer {
+
+	void setFaceletContext(FaceletContext faceletContext);
+	
+}
---
+++ src/main/java/org/omnifaces/taghandler/ComponentExtraHandler.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2014 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.taghandler;
+
+import javax.faces.view.facelets.ComponentConfig;
+import javax.faces.view.facelets.ComponentHandler;
+import javax.faces.view.facelets.FaceletContext;
+
+import org.omnifaces.component.util.FaceletContextConsumer;
+
+/**
+ * Handler that can be used by components which wish to receive various extra services.
+ * <p>
+ * Those extra services consist of:
+ * 
+ * <ul>
+ * <li> Receiving the {@link FaceletContext} for the Facelet in which the component appears
+ * </ul>
+ * 
+ * <p>
+ * The handler has to be used alongside a component declaration in a Facelets <code>*-taglib.xml</code>, e.g.
+ * 
+ * <pre>
+ * 	&lt;tag&gt;
+ *		&lt;tag-name&gt;someComponent&lt;/tag-name&gt;
+ *		&lt;component&gt;
+ *			&lt;component-type&gt;com.example.SomeComponent&lt;/component-type&gt;
+ *			&lt;handler-class&gt;org.omnifaces.taghandler.ComponentExtraHandler&lt;/handler-class&gt;
+ *		&lt;/component&gt;
+ *	&lt;/tag&gt;
+ * </pre>
+ * 
+ * @since 2.0
+ * @author Arjan Tijms
+ * 
+ */
+public class ComponentExtraHandler extends ComponentHandler {
+
+	public ComponentExtraHandler(ComponentConfig config) {
+		super(config);
+	}
+
+	@Override
+	public void setAttributes(FaceletContext ctx, Object component) {
+		super.setAttributes(ctx, component);
+
+		if (component instanceof FaceletContextConsumer) {
+			FaceletContextConsumer faceletContextConsumer = (FaceletContextConsumer) component;
+
+			faceletContextConsumer.setFaceletContext(ctx);
+		}
+
+	}
+
+}
