916123efb0a94967c12101f3eacd9e7806dfbfb9
Bauke Scholtz <bauke.scholtz@kizitos.com>
2016-01-28 23:13:17 +0100

    Improve and document o:skipValidators.

--- src/main/java/org/omnifaces/taghandler/SkipValidators.java
+++ src/main/java/org/omnifaces/taghandler/SkipValidators.java
@@ -1,12 +1,33 @@
+/*
+ * Copyright 2016 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.omnifaces.taghandler;
 
+import static java.lang.Boolean.TRUE;
+import static javax.faces.event.PhaseId.RESTORE_VIEW;
+import static org.omnifaces.util.Components.hasInvokedSubmit;
+import static org.omnifaces.util.Events.subscribeToRequestAfterPhase;
+import static org.omnifaces.util.Events.subscribeToViewEvent;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.component.UICommand;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
+import javax.faces.component.behavior.ClientBehaviorHolder;
+import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
-import javax.faces.event.ComponentSystemEvent;
-import javax.faces.event.ComponentSystemEventListener;
-import javax.faces.event.PostAddToViewEvent;
 import javax.faces.event.PostValidateEvent;
 import javax.faces.event.PreValidateEvent;
 import javax.faces.event.SystemEvent;
@@ -16,122 +37,136 @@ import javax.faces.view.facelets.ComponentHandler;
 import javax.faces.view.facelets.FaceletContext;
 import javax.faces.view.facelets.TagConfig;
 import javax.faces.view.facelets.TagHandler;
-import org.omnifaces.util.Components;
-import org.omnifaces.util.Events;
-import org.omnifaces.util.Faces;
 
+import org.omnifaces.util.Callback;
 
 /**
- * A tag handler to skip validation.
- * 
+ * <p>
+ * The <code>&lt;o:skipValidators&gt;</code> taghandler allows the developer to entirely skip validation when
+ * executing an {@link UICommand} or {@link ClientBehaviorHolder} action. This taghandler must be placed inside an
+ * {@link UICommand} or {@link ClientBehaviorHolder} component (client behavior holder components are those components
+ * supporting <code>&lt;f:ajax&gt;</code>).
+ *
+ * <h3>Usage</h3>
+ * <p>
+ * For example, when adding a new row to the data table, you'd like to not immediately validate all empty rows.
+ * <pre>
+ * &lt;h:form&gt;
+ *     &lt;h:dataTable value="#{bean.items}" var="item"&gt;
+ *         &lt;h:column&gt;
+ *             &lt;h:inputText value="#{item.value}" required="true" /&gt;
+ *         &lt;/h:column&gt;
+ *     &lt;/h:dataTable&gt;
+ *     &lt;h:commandButton value="add new row" action="#{bean.add}"&gt;
+ *         &lt;o:skipValidators /&gt;
+ *     &lt;/h:commandButton&gt;
+ *     &lt;h:commandButton value="save all data" action="#{bean.save}" /&gt;
+ *     &lt;h:messages /&gt;
+ * &lt;/h:form&gt;
+ * </pre>
+ * <p>
+ * Note that converters will still run and that model values will still be updated. This behavior is by design.
+ *
  * @author Michele Mariotti
+ * @author Bauke Scholtz
+ * @since 2.3
  */
-public class SkipValidators extends TagHandler
-{
-    public static final String SKIP_VALIDATORS = "org.omnifaces.SKIP_VALIDATORS";
+public class SkipValidators extends TagHandler {
 
-    public SkipValidators(TagConfig config)
-    {
-        super(config);
-    }
+	// Constants ------------------------------------------------------------------------------------------------------
 
-    @Override
-    public void apply(final FaceletContext context, final UIComponent parent) throws IOException
-    {
-        if(ComponentHandler.isNew(parent))
-        {
-            parent.subscribeToEvent(PostAddToViewEvent.class, new PostAddToViewEventListener());
+	private static final String ERROR_INVALID_PARENT =
+		"Parent component of o:skipValidators must be an instance of UICommand or ClientBehaviorHolder.";
 
-            Events.subscribeToViewEvent(PreValidateEvent.class, new PreValidateEventListener());
-            Events.subscribeToViewEvent(PostValidateEvent.class, new PostValidateEventListener());
-        }
-    }
+	// Constructors ---------------------------------------------------------------------------------------------------
 
-    /**
-     * Check if the given component has been invoked during the current request and if so, set a flag to skip validators.
-     */
-    public static class PostAddToViewEventListener implements ComponentSystemEventListener
-    {
-        @Override
-        public void processEvent(ComponentSystemEvent event) throws AbortProcessingException
-        {
-            UIComponent component = event.getComponent();
+	/**
+	 * The tag constructor.
+	 * @param config The tag config.
+	 */
+	public SkipValidators(TagConfig config) {
+		super(config);
+	}
 
-            if(Components.hasInvokedSubmit(component))
-            {
-                Faces.setContextAttribute(SKIP_VALIDATORS, true);
-            }
-        }
-    }
+	// Actions --------------------------------------------------------------------------------------------------------
 
-    /**
-     * Remove validators and save them to be restored later.
-     */
-    public static class PreValidateEventListener implements SystemEventListener
-    {
-        @Override
-        public void processEvent(SystemEvent event) throws AbortProcessingException
-        {
-            UIInput input = (UIInput) event.getSource();
+	/**
+	 * If the parent component is an instance of {@link UICommand} or {@link ClientBehaviorHolder}, and is new, and
+	 * we're in the restore view phase of a postback, then delegate to {@link #processSkipValidators(UIComponent)}.
+	 * @throws IllegalArgumentException When the parent component is not an instance of {@link UICommand} or
+	 * {@link ClientBehaviorHolder}.
+	 */
+	@Override
+	public void apply(FaceletContext context, final UIComponent parent) throws IOException {
+		if (!(parent instanceof UICommand || parent instanceof ClientBehaviorHolder)) {
+			throw new IllegalArgumentException(ERROR_INVALID_PARENT);
+		}
 
-            boolean skipValidators = Faces.getContextAttribute(SKIP_VALIDATORS) == Boolean.TRUE;
+		FacesContext facesContext = context.getFacesContext();
 
-            if(skipValidators)
-            {
-                Faces.setContextAttribute(input.getClientId() + ".required", input.isRequired());
-                input.setRequired(false);
+		if (!(ComponentHandler.isNew(parent) && facesContext.isPostback() && facesContext.getCurrentPhaseId() == RESTORE_VIEW)) {
+			return;
+		}
 
-                Validator[] validators = input.getValidators();
-                if(validators != null)
-                {
-                    Faces.setContextAttribute(input.getClientId() + ".validators", validators);
-                    for(Validator validator : validators)
-                    {
-                        input.removeValidator(validator);
-                    }
-                }
-            }
-        }
+		// We can't use hasInvokedSubmit() before the component is added to view, because the client ID isn't available.
+		// Hence, we subscribe this check to after phase of restore view.
+		subscribeToRequestAfterPhase(RESTORE_VIEW, new Callback.Void() { @Override public void invoke() {
+			processSkipValidators(parent);
+		}});
+	}
 
-        @Override
-        public boolean isListenerForSource(Object source)
-        {
-            return source instanceof UIInput;
-        }
-    }
+	/**
+	 * Check if the given component has been invoked during the current request and if so, then register the skip
+	 * validators event listener which removes the validators during {@link PreValidateEvent} and restores them during
+	 * {@link PostValidateEvent}.
+	 */
+	protected void processSkipValidators(UIComponent parent) {
+		if (!hasInvokedSubmit(parent)) {
+			return;
+		}
 
-    /**
-     * Restore previously saved validators.
-     */
-    public static class PostValidateEventListener implements SystemEventListener
-    {
-        @Override
-        public void processEvent(SystemEvent event) throws AbortProcessingException
-        {
-            UIInput input = (UIInput) event.getSource();
+		SkipValidatorsEventListener listener = new SkipValidatorsEventListener();
+		subscribeToViewEvent(PreValidateEvent.class, listener);
+		subscribeToViewEvent(PostValidateEvent.class, listener);
+	}
 
-            boolean skipValidators = Faces.getContextAttribute(SKIP_VALIDATORS) == Boolean.TRUE;
+	/**
+	 * Remove validators during prevalidate and restore them during postvalidate.
+	 */
+	static class SkipValidatorsEventListener implements SystemEventListener {
 
-            if(skipValidators)
-            {
-                boolean required = Faces.getContextAttribute(input.getClientId() + ".required") == Boolean.TRUE;
-                input.setRequired(required);
+		private Map<String, Boolean> required = new HashMap<>();
+		private Map<String, Validator[]> allValidators = new HashMap<>();
 
-                Validator[] validators = Faces.getContextAttribute(input.getClientId() + ".validators");
-                if(validators != null)
-                {
-                    for(Validator validator : validators)
-                    {
-                        input.addValidator(validator);
-                    }
-                }
-            }
-        }
+		@Override
+		public boolean isListenerForSource(Object source) {
+			return source instanceof UIInput;
+		}
 
-        @Override
-        public boolean isListenerForSource(Object source)
-        {
-            return source instanceof UIInput;
-        }
-    }
+		@Override
+		public void processEvent(SystemEvent event) throws AbortProcessingException {
+			UIInput input = (UIInput) event.getSource();
+			String clientId = input.getClientId();
+
+			if (event instanceof PreValidateEvent) {
+				required.put(clientId, input.isRequired());
+				input.setRequired(false);
+				Validator[] validators = input.getValidators();
+				allValidators.put(clientId, validators);
+
+				for (Validator validator : validators) {
+					input.removeValidator(validator);
+				}
+			}
+			else if (event instanceof PostValidateEvent) {
+				input.setRequired(TRUE.equals(required.remove(clientId)));
+
+				for (Validator validator : allValidators.remove(clientId)) {
+					input.addValidator(validator);
+				}
+			}
+		}
+
+	}
+
 }
