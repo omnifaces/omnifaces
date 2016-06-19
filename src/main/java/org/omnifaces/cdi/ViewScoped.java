8d91396091e52f02af30d982271db9a0f751cee6
Bauke Scholtz <bauke.scholtz@kizitos.com>
2016-06-19 18:24:14 +0200

    Clarified javadoc.

--- src/main/java/org/omnifaces/cdi/ViewScoped.java
+++ src/main/java/org/omnifaces/cdi/ViewScoped.java
@@ -57,7 +57,7 @@ import org.omnifaces.viewhandler.RestorableViewHandler;
  * Since OmniFaces 2.3, the unload has been further improved to also physically remove the associated JSF view state
  * from JSF implementation's internal LRU map in case of server side state saving, hereby further decreasing the risk
  * at <code>ViewExpiredException</code> on the other views which were created/opened earlier. As side effect of this
- * change, the <code>&#64;PreDestroy</code> annotated method of any regular JSF CDI view scoped beans referenced in the
+ * change, the <code>&#64;PreDestroy</code> annotated method of any standard JSF view scoped beans referenced in the
  * same view as the OmniFaces CDI view scoped bean will also guaranteed be invoked on browser unload.
  * <p>
  * In a nutshell: if you're on JSF 2.0/2.1, and you can't upgrade to JSF 2.2, and you want the
