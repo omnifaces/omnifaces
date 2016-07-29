ca4dd40619e0e2145d43a30f0e1fcad9d7558ff0
Bauke Scholtz <bauke.scholtz@kizitos.com>
2016-07-29 13:01:48 +0200

    #281: deprecate BeansLocal as it has no sense with CDI#current()

--- src/main/java/org/omnifaces/util/BeansLocal.java
+++ src/main/java/org/omnifaces/util/BeansLocal.java
@@ -27,6 +27,7 @@ import javax.enterprise.inject.Typed;
 import javax.enterprise.inject.spi.Annotated;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 import org.omnifaces.cdi.beans.InjectionPointGenerator;
@@ -42,8 +43,10 @@ import org.omnifaces.cdi.beans.InjectionPointGenerator;
  *
  * @author Bauke Scholtz
  * @since 2.0
+ * @deprecated Since 2.5 As the {@link Beans} utility class now uses {@link CDI#current()} which is already more efficient.
  */
 @Typed
+@Deprecated // TODO: remove in OmniFaces 3.0.
 public final class BeansLocal {
 
 	// Constants ------------------------------------------------------------------------------------------------------
