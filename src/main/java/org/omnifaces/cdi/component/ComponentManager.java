cf4000f70233282d8bbe13ce2180b61e9fbd2ea5
Juliano Marques <juliano@ibasi.com.br>
2016-08-17 09:46:24 -0300

    #302 Correcting messed up tabs

--- src/main/java/org/omnifaces/cdi/component/ComponentManager.java
+++ src/main/java/org/omnifaces/cdi/component/ComponentManager.java
@@ -13,76 +13,76 @@ import javax.inject.Inject;
 @ApplicationScoped
 public class ComponentManager {
 
-    @Inject
-    private BeanManager manager;
-    private Map<String, Bean<UIComponent>> componentsByType = new HashMap<>();
+	@Inject
+	private BeanManager manager;
+	private Map<String, Bean<UIComponent>> componentsByType = new HashMap<>();
 
-    @SuppressWarnings("unchecked")
-    public UIComponent createComponent(Application application, FacesContext context, String componentType, String rendererType) {
-	Bean<UIComponent> bean = componentsByType.get(componentType);
+	@SuppressWarnings("unchecked")
+	public UIComponent createComponent(Application application, FacesContext context, String componentType, String rendererType) {
+		Bean<UIComponent> bean = componentsByType.get(componentType);
 
-	if (bean == null && !componentsByType.containsKey(componentType)) {
-	    UIComponent component = application.createComponent(context, componentType, rendererType);
+		if (bean == null && !componentsByType.containsKey(componentType)) {
+			UIComponent component = application.createComponent(context, componentType, rendererType);
 
-	    if (component != null) {
-		bean = (Bean<UIComponent>) resolve(manager, component.getClass());
-	    }
+			if (component != null) {
+				bean = (Bean<UIComponent>) resolve(manager, component.getClass());
+			}
 
-	    componentsByType.put(componentType, bean);
+			componentsByType.put(componentType, bean);
+		}
+
+		return bean != null ? getReference(manager, bean) : null;
 	}
 
-	return bean != null ? getReference(manager, bean) : null;
-    }
+	@SuppressWarnings("unchecked")
+	public UIComponent createComponent(Application application, String componentType) {
+		Bean<UIComponent> bean = componentsByType.get(componentType);
 
-    @SuppressWarnings("unchecked")
-    public UIComponent createComponent(Application application, String componentType) {
-	Bean<UIComponent> bean = componentsByType.get(componentType);
+		if (bean == null && !componentsByType.containsKey(componentType)) {
+			UIComponent component = application.createComponent(componentType);
 
-	if (bean == null && !componentsByType.containsKey(componentType)) {
-	    UIComponent component = application.createComponent(componentType);
+			if (component != null) {
+				bean = (Bean<UIComponent>) resolve(manager, component.getClass());
+			}
 
-	    if (component != null) {
-		bean = (Bean<UIComponent>) resolve(manager, component.getClass());
-	    }
+			componentsByType.put(componentType, bean);
+		}
 
-	    componentsByType.put(componentType, bean);
+		return bean != null ? getReference(manager, bean) : null;
 	}
 
-	return bean != null ? getReference(manager, bean) : null;
-    }
+	@SuppressWarnings("unchecked")
+	public UIComponent createComponent(Application application, ValueExpression componentExpression, FacesContext context, String componentType, String rendererType) {
+		Bean<UIComponent> bean = componentsByType.get(componentType);
 
-    @SuppressWarnings("unchecked")
-    public UIComponent createComponent(Application application, ValueExpression componentExpression, FacesContext context, String componentType, String rendererType) {
-	Bean<UIComponent> bean = componentsByType.get(componentType);
+		if (bean == null && !componentsByType.containsKey(componentType)) {
+			UIComponent component = application.createComponent(componentExpression, context, componentType, rendererType);
 
-	if (bean == null && !componentsByType.containsKey(componentType)) {
-	    UIComponent component = application.createComponent(componentExpression, context, componentType, rendererType);
+			if (component != null) {
+				bean = (Bean<UIComponent>) resolve(manager, component.getClass());
+			}
 
-	    if (component != null) {
-		bean = (Bean<UIComponent>) resolve(manager, component.getClass());
-	    }
+			componentsByType.put(componentType, bean);
+		}
 
-	    componentsByType.put(componentType, bean);
+		return bean != null ? getReference(manager, bean) : null;
 	}
 
-	return bean != null ? getReference(manager, bean) : null;
-    }
+	@SuppressWarnings("unchecked")
+	public UIComponent createComponent(Application application, ValueExpression componentExpression, FacesContext context, String componentType) {
+		Bean<UIComponent> bean = componentsByType.get(componentType);
 
-    @SuppressWarnings("unchecked")
-    public UIComponent createComponent(Application application, ValueExpression componentExpression, FacesContext context, String componentType) {
-	Bean<UIComponent> bean = componentsByType.get(componentType);
+		if (bean == null && !componentsByType.containsKey(componentType)) {
+			UIComponent component = application.createComponent(componentExpression, context, componentType);
 
-	if (bean == null && !componentsByType.containsKey(componentType)) {
-	    UIComponent component = application.createComponent(componentExpression, context, componentType);
+			if (component != null) {
+				bean = (Bean<UIComponent>) resolve(manager, component.getClass());
+			}
 
-	    if (component != null) {
-		bean = (Bean<UIComponent>) resolve(manager, component.getClass());
-	    }
+			componentsByType.put(componentType, bean);
+		}
 
-	    componentsByType.put(componentType, bean);
+		return bean != null ? getReference(manager, bean) : null;
 	}
-
-	return bean != null ? getReference(manager, bean) : null;
-    }
 
 }
