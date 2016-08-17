dd766bd7cc1a5a7c9b70ab61628a9dcd4f05b8fa
Bauke Scholtz <bauke.scholtz@kizitos.com>
2016-08-17 23:46:51 +0200

    Add integration test for @ViewScoped

---
+++ src/test/java/org/omnifaces/test/cdi/ViewScopedIT.java
@@ -0,0 +1,130 @@
+package org.omnifaces.test.cdi;
+
+import static org.jboss.arquillian.graphene.Graphene.guardAjax;
+import static org.jboss.arquillian.graphene.Graphene.guardHttp;
+import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
+import static org.jboss.shrinkwrap.api.asset.EmptyAsset.INSTANCE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.File;
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@RunWith(Arquillian.class)
+public class ViewScopedIT {
+
+	@Drone
+	private WebDriver browser;
+
+    @ArquillianResource
+    private URL contextPath;
+
+    @FindBy(id="bean")
+    private WebElement bean;
+
+    @FindBy(id="messages")
+    private WebElement messages;
+
+    @FindBy(id="refresh")
+    private WebElement refresh;
+
+    @FindBy(id="non-ajax:submit")
+    private WebElement nonAjaxSubmit;
+
+    @FindBy(id="non-ajax:navigate")
+    private WebElement nonAjaxNavigate;
+
+    @FindBy(id="non-ajax:rebuild")
+    private WebElement nonAjaxRebuild;
+
+    @FindBy(id="ajax:submit")
+    private WebElement ajaxSubmit;
+
+    @FindBy(id="ajax:navigate")
+    private WebElement ajaxNavigate;
+
+    @FindBy(id="ajax:rebuild")
+    private WebElement ajaxRebuild;
+
+    @Deployment(testable = false)
+	public static Archive<?> createDeployment() {
+		return create(WebArchive.class, "ViewScopedIT.war")
+			.addClass(ViewScopedITBean.class)
+			.addAsWebResource("cdi/ViewScopedIT.xhtml")
+			.addAsWebInfResource(INSTANCE, "beans.xml")
+			.setWebXML("web.xml")
+			.addAsLibrary(new File("target/" + System.getProperty("finalName") + ".jar"));
+	}
+
+    @Test
+	public void test(String formId) {
+		browser.get(contextPath + "ViewScopedIT.xhtml");
+		assertEquals("init", messages.getText());
+		String previousBean = bean.getText();
+
+		guardHttp(refresh).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals("unload init", messages.getText());
+
+		guardHttp(nonAjaxSubmit).click();
+		assertEquals(previousBean, previousBean = bean.getText());
+		assertEquals("submit", messages.getText());
+
+		guardHttp(nonAjaxNavigate).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals("navigate destroy init", messages.getText());
+
+		guardHttp(nonAjaxSubmit).click();
+		assertEquals(previousBean, previousBean = bean.getText());
+		assertEquals("submit", messages.getText());
+
+		guardHttp(nonAjaxRebuild).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals("rebuild destroy init", messages.getText());
+
+		guardHttp(nonAjaxSubmit).click();
+		assertEquals(previousBean, previousBean = bean.getText());
+		assertEquals("submit", messages.getText());
+
+		guardHttp(refresh).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals(messages.getText(), "unload init");
+
+		guardAjax(ajaxSubmit).click();
+		assertEquals(previousBean, previousBean = bean.getText());
+		assertEquals("submit", messages.getText());
+
+		guardAjax(ajaxNavigate).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals("navigate destroy init", messages.getText());
+
+		guardAjax(ajaxSubmit).click();
+		assertEquals(previousBean, previousBean = bean.getText());
+		assertEquals("submit", messages.getText());
+
+		guardAjax(ajaxRebuild).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals("rebuild destroy init", messages.getText());
+
+		guardAjax(ajaxSubmit).click();
+		assertEquals(previousBean, previousBean = bean.getText());
+		assertEquals("submit", messages.getText());
+
+		guardHttp(refresh).click();
+		assertNotEquals(previousBean, previousBean = bean.getText());
+		assertEquals("unload init", messages.getText());
+    }
+
+}
---
+++ src/test/java/org/omnifaces/test/cdi/ViewScopedITBean.java
@@ -0,0 +1,65 @@
+package org.omnifaces.test.cdi;
+
+import static org.omnifaces.cdi.viewscope.ViewScopeManager.isUnloadRequest;
+import static org.omnifaces.util.Faces.getContext;
+import static org.omnifaces.util.Faces.getViewId;
+import static org.omnifaces.util.Faces.hasContext;
+import static org.omnifaces.util.Faces.setViewRoot;
+import static org.omnifaces.util.Messages.addGlobalInfo;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Named;
+
+import org.omnifaces.cdi.ViewScoped;
+
+@Named
+@ViewScoped
+public class ViewScopedITBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static boolean unloaded;
+	private static boolean destroyed;
+
+	@PostConstruct
+	public void init() {
+		if (unloaded) {
+			addGlobalInfo("unload");
+			unloaded = false;
+		}
+		else if (destroyed) {
+			addGlobalInfo("destroy");
+			destroyed = false;
+		}
+
+		addGlobalInfo("init");
+	}
+
+	public void submit() {
+		addGlobalInfo("submit");
+	}
+
+	public String navigate() {
+		addGlobalInfo("navigate");
+		return getViewId();
+	}
+
+    public void rebuild() {
+		addGlobalInfo("rebuild");
+    	setViewRoot(getViewId());
+    }
+
+    @PreDestroy
+    public void destroy() {
+    	if (hasContext() && isUnloadRequest(getContext())) {
+    		unloaded = true;
+    	}
+    	else {
+    		destroyed = true;
+    	}
+    }
+
+}
