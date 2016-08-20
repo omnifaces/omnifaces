264c73702a9f20c037bb0d15d798103d34e84037
Bauke Scholtz <bauke.scholtz@kizitos.com>
2016-08-20 23:04:29 +0200

    Run license header maven plugin

--- src/main/java/org/omnifaces/util/concurrentlinkedhashmap/package-info.java
+++ src/main/java/org/omnifaces/util/concurrentlinkedhashmap/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Google Inc. All Rights Reserved.
+ * Copyright 2016 OmniFaces
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,10 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * REPACKAGED BY OMNIFACES, BUT NO CODE CHANGES HAVE BEEN MADE.
  */
-
 /**
  * This package contains an implementation of a bounded
  * {@link java.util.concurrent.ConcurrentMap} data structure.
