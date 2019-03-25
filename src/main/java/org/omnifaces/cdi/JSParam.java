716a160ff4ede61f07350ca97f01aac7eb5994c2
Adam Waldenberg <adam.waldenberg@ejwa.se>
2019-03-25 10:24:15 +0100

    Add documentation over complex types and how it works with @JSParam

--- src/main/java/org/omnifaces/cdi/JSParam.java
+++ src/main/java/org/omnifaces/cdi/JSParam.java
@@ -28,6 +28,7 @@ import java.lang.annotation.Target;
  *
  * <h3>Usage</h3>
  *
+ * <h4>Simple types</h4>
  * <p>
  * The example below injects the time zone offset of the client browser into <code>timezoneOffset</code>.
  * <pre>
@@ -40,6 +41,39 @@ import java.lang.annotation.Target;
  * &#64;Inject &#64;JSParam("window.screen.width")
  * private int screenWidth;
  * </pre>
+ *
+ * <h4>Complex types</h4>
+ * <p>
+ * <code>&#64;</code>{@link JSParam} also supports injection of complex JavaScript types. To make this work, an
+ * equivalent Java class needs to be defined and used as the injection point. The implementation uses Jackson to handle
+ * the mapping onto the Java object - so the defined class can use any of the Jackson specific JSON annotations to
+ * control the conversion.
+ * <pre>
+ * @JsonIgnoreProperties(ignoreUnknown = true)
+ * public static class Navigator {
+ *     @JsonProperty private String vendor;
+ *     @JsonProperty private String userAgent;
+ *     @JsonProperty private String language;
+ *
+ *     public getVendor() {
+ *         return vendor;
+ *     }
+ *
+ *     public getUserAgent() {
+ *         return userAgent;
+ *     }
+ *
+ *     public getLanguage() {
+ *         return language;
+ *     }
+ * }
+ *
+ * &#64;Inject &#64;JSParam("navigator")
+ * private Navigator navigator;
+ * </p>
+ * <p>
+ * Note that the implementation only traverses complex JavaScript types one level. However, if you need to recursively
+ * collect certain data, this can be done with some custom JavaScript code.
  *
  * @since 3.3
  * @author Adam Waldenberg, Ejwa Software/Hosting
