e10c03fa4dfaa03a42dcc86b468f072f66b85b48
BalusC <balusc@gmail.com>
2019-05-04 12:19:29 -0400

    Fix #504

--- src/main/java/org/omnifaces/ApplicationProcessor.java
+++ src/main/java/org/omnifaces/ApplicationProcessor.java
@@ -12,13 +12,18 @@
  */
 package org.omnifaces;
 
+import static java.lang.String.format;
 import static java.util.logging.Level.SEVERE;
 import static org.omnifaces.ApplicationInitializer.ERROR_OMNIFACES_INITIALIZATION_FAIL;
 import static org.omnifaces.util.Faces.getServletContext;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.faces.application.Application;
+import javax.faces.application.ResourceHandler;
+import javax.faces.application.ResourceHandlerWrapper;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
 import javax.servlet.ServletContext;
@@ -31,6 +36,7 @@ import org.omnifaces.facesviews.FacesViews;
  * OmniFaces application processor. This runs when the faces application is created.
  * This performs the following tasks:
  * <ol>
+ * <li>Check if {@link Application#getResourceHandler()} chain is unique, otherwise log and fail.
  * <li>Register the {@link FacesViews} view handler.
  * <li>Register the {@link MessagesKeywordResolver}.
  * </ol>
@@ -46,6 +52,11 @@ public class ApplicationProcessor implements SystemEventListener {
 
 	private static final Logger logger = Logger.getLogger(ApplicationProcessor.class.getName());
 
+	private static final String ERROR_DUPLICATE_RESOURCE_HANDLER =
+		"Resource handler %s is duplicated."
+			+ " This will result in erratic resource handling behavior."
+			+ " Please check your faces-config.xml files, also in web fragment JAR files.";
+
 	// Actions --------------------------------------------------------------------------------------------------------
 
 	@Override
@@ -58,12 +69,31 @@ public class ApplicationProcessor implements SystemEventListener {
 		try {
 			ServletContext servletContext = getServletContext();
 			Application application = (Application) event.getSource();
+			checkDuplicateResourceHandler(application);
 			FacesViews.registerViewHander(servletContext, application);
 			MessagesKeywordResolver.register(application);
 		}
 		catch (Exception | LinkageError e) {
 			logger.log(SEVERE, ERROR_OMNIFACES_INITIALIZATION_FAIL, e);
 			throw e;
+		}
+	}
+
+	private void checkDuplicateResourceHandler(Application application) {
+		Set<Class<? extends ResourceHandler>> allResourceHandlers = new HashSet<>();
+		ResourceHandler resourceHandler = application.getResourceHandler();
+
+		while (resourceHandler != null) {
+			if (!allResourceHandlers.add(resourceHandler.getClass())) {
+				throw new IllegalStateException(format(ERROR_DUPLICATE_RESOURCE_HANDLER, resourceHandler.getClass()));
+			}
+
+			if (resourceHandler instanceof ResourceHandlerWrapper) {
+				resourceHandler = ((ResourceHandlerWrapper) resourceHandler).getWrapped();
+			}
+			else {
+				resourceHandler = null;
+			}
 		}
 	}
 
