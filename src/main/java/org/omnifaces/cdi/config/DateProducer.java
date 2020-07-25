9fa9f6ced3ee94fe490344efbb1acd3a8d46c001
BalusC <balusc@gmail.com>
2020-07-25 08:17:49 -0400

    Improve #{now} and #{startup}

--- src/main/java/org/omnifaces/cdi/config/DateProducer.java
+++ src/main/java/org/omnifaces/cdi/config/DateProducer.java
@@ -12,7 +12,14 @@
  */
 package org.omnifaces.cdi.config;
 
+import static org.omnifaces.util.Utils.toZonedDateTime;
+
+import java.io.Serializable;
 import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -26,30 +33,170 @@ import org.omnifaces.cdi.Eager;
  * <p>
  * Producer for <code>#{startup}</code> and <code>#{now}</code>.
  * <p>
- * Since 4.0 it produces by default {@link Instant} whereas it previously produced {@link Date}.
+ * Since 4.0 it produces by default an instance of {@link Temporal} whereas it previously produced an instance of
+ * {@link Date}.
  * <p>
- * Historical note: since 1.0 these were registered as beans in faces-config.xml. Since 3.5.1 these were migrated to
+ * Historical note: since 1.0 these were registered as beans in faces-config.xml. Since 3.6 these were migrated to
  * CDI producers, because the CDI implementation being used may emit warnings on them not being proxyable.
  *
+ * <h2>Usage</h2>
+ * <ul>
+ * <li>You can reference the application startup time as {@link Temporal} via <code>#{startup}</code> in EL.</li>
+ * <li>You can reference the current time as {@link Temporal} via <code>#{now}</code> in EL.</li>
+ * <li>They have a {@link TemporalDate#getTime()} method which allows you to obtain the epoch time via
+ * <code>#{startup.time}</code> and <code>#{now.time}</code> in EL.</li>
+ * <li>They have a {@link TemporalDate#getInstant()} method which allows you to convert them to {@link Instant} via
+ * <code>#{startup.instant}</code> and <code>#{now.instant}</code> in EL.</li>
+ * <li>They have a {@link TemporalDate#getZonedDateTime()} method which allows you to convert them to
+ * {@link ZonedDateTime} via <code>#{startup.zonedDateTime}</code> and <code>#{now.zonedDateTime}</code> in EL.</li>
+ * <li>They are injectable in CDI beans via e.g. <code>&#64;Inject &#64;Named private Temporal startup;</code>.
+ * </ul>
+ *
  * @author Bauke Scholtz
- * @since 3.5.1
+ * @since 3.6
  */
 public class DateProducer {
 
 	/**
-	 * This makes an instance of {@link Instant} as startup datetime available by <code>#{startup}</code>.
+	 * This makes an instance of {@link Temporal} as startup datetime available by <code>#{startup}</code>.
 	 */
 	@Produces @Named @ApplicationScoped @Eager
-	public Instant getStartup() {
-		return Instant.now();
+	public TemporalDate getStartup() {
+		return new TemporalDate();
 	}
 
 	/**
-	 * This makes an instance of {@link Instant} as current datetime available by <code>#{now}</code>.
+	 * This makes an instance of {@link Temporal} as current datetime available by <code>#{now}</code>.
 	 */
 	@Produces @Named @RequestScoped
-	public Instant getNow() {
-		return Instant.now();
+	public TemporalDate getNow() {
+		return new TemporalDate();
 	}
 
+	/**
+	 * {@link Instant} is a final class, hence this proxy for CDI. Plus, it also offers a fallback for existing EL
+	 * expressions relying on {@link Date#getTime()} such as <code>#{now.time}</code> so that they continue working
+	 * after migration from {@link Date} to {@link Temporal} in OmniFaces 4.0.
+	 *
+	 * @author Bauke Scholtz
+	 * @since 4.0
+	 */
+	public static class TemporalDate implements Temporal, Comparable<TemporalDate>, Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		private Instant instant;
+		private ZonedDateTime zonedDateTime;
+		private long time;
+		private String string;
+
+		/**
+		 * Constructs a new proxyable instant which is initialized with {@link Instant#now()}.
+		 */
+		public TemporalDate() {
+			this(Instant.now());
+		}
+
+		/**
+		 * Constructs a new proxyable instant which is initialized with given {@link Instant}.
+		 */
+		public TemporalDate(Instant instant) {
+			this.instant = instant;
+			this.zonedDateTime = toZonedDateTime(instant);
+			this.time = instant.toEpochMilli();
+			this.string = instant.toString();
+		}
+
+		/**
+		 * Convenience method to return this as {@link Instant}.
+		 * @return This as {@link Instant}.
+		 */
+		public Instant getInstant() {
+			return instant;
+		}
+
+		/**
+		 * Convenience method to return this as {@link ZonedDateTime}.
+		 * @return This as {@link ZonedDateTime}.
+		 */
+		public ZonedDateTime getZonedDateTime() {
+			return zonedDateTime;
+		}
+
+		/**
+		 * Has the same signature as {@link Date#getTime()}.
+		 * This ensures that <code>#{now.time}</code> and <code>#{startup.time}</code> keep working.
+		 * @return The number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this instant.
+		 */
+		public long getTime() {
+			return time;
+		}
+
+		// Required overrides -----------------------------------------------------------------------------------------
+
+		/**
+		 * Returns {@link Instant#compareTo(Instant)}
+		 */
+		@Override
+		public int compareTo(TemporalDate other) {
+			return instant.compareTo(other.getInstant());
+		}
+
+		/**
+		 * Returns {@link Instant#equals(Object)}
+		 */
+		@Override
+		public boolean equals(Object other) {
+			return instant.equals(other);
+		}
+
+		/**
+		 * Returns {@link Instant#hashCode()}
+		 */
+		@Override
+		public int hashCode() {
+			return instant.hashCode();
+		}
+
+		/**
+		 * Returns {@link Instant#toString()}.
+		 */
+		@Override
+		public String toString() {
+			return string;
+		}
+
+		// Temporal delegates -----------------------------------------------------------------------------------------
+
+		@Override
+		public boolean isSupported(TemporalField field) {
+			return instant.isSupported(field);
+		}
+
+		@Override
+		public long getLong(TemporalField field) {
+			return instant.getLong(field);
+		}
+
+		@Override
+		public boolean isSupported(TemporalUnit unit) {
+			return instant.isSupported(unit);
+		}
+
+		@Override
+		public Temporal with(TemporalField field, long newValue) {
+			return instant.with(field, newValue);
+		}
+
+		@Override
+		public Temporal plus(long amountToAdd, TemporalUnit unit) {
+			return instant.plus(amountToAdd, unit);
+		}
+
+		@Override
+		public long until(Temporal endExclusive, TemporalUnit unit) {
+			return instant.until(endExclusive, unit);
+		}
+
+	}
 }
