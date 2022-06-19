674ad1734f4d128fe2b9831bd74e6dfcfdd16c8d
Bauke Scholtz <balusc@gmail.com>
2022-06-19 13:45:15 -0400

    Remove dangling sysout lines

--- src/test/java/org/omnifaces/test/cdi/config/TestDateProducer.java
+++ src/test/java/org/omnifaces/test/cdi/config/TestDateProducer.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright OmniFaces
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.omnifaces.test.cdi.config;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,9 +77,6 @@ public class TestDateProducer {
 		long oldTime = oldNow.getTime();
 		long newTime = newNowAsInstant.toEpochMilli();
 		long timeDiff = Math.abs(oldTime - newTime);
-
-		System.out.println(oldNow);
-		System.out.println(newNowAsInstant);
 
 		assertTrue(timeDiff < TimeUnit.SECONDS.toMillis(1), timeDiff + " is less than 1s");
 	}
