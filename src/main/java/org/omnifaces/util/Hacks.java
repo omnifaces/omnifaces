56ab51ff23ca5ed5b933b62bd5a74d4121efcdae
Bauke Scholtz <balusc@gmail.com>
2023-06-24 12:14:23 -0400

    Fixed/improved wording in disclaimer of internal utility classes

--- src/main/java/org/omnifaces/util/Hacks.java
+++ src/main/java/org/omnifaces/util/Hacks.java
@@ -61,9 +61,9 @@ import jakarta.websocket.Session;
  * This class is indeed intented for internal usage only. We won't add methods here on user request. We only add methods
  * here once we encounter non-DRY code in OmniFaces codebase. The methods may be renamed/changed without notice.
  * <p>
- * We don't stop you from using it if you found it in the Javadoc and you think you find it useful, but there is no
- * guarantee that method signatures will be changed without notice. This utility class exists because OmniFaces intends
- * to be free of 3rd party dependencies.
+ * We don't stop you from using it if you found it in the Javadoc and you think you find it useful, but you have to
+ * accept the risk that the method signatures can be changed without notice. This utility class exists because OmniFaces
+ * intends to be free of 3rd party dependencies.
  *
  * @author Bauke Scholtz
  * @author Arjan Tijms
