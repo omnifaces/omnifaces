51e243b0c593f9eb3f64a2fe303a8fae35dca940
pizzi80 <paolo@given2.com>
2024-05-06 16:05:21 +0200

    Concurrency utilities
    Signed-off-by: pizzi80 <paolo@given2.com>

--- src/main/java/org/omnifaces/util/Utils.java
+++ src/main/java/org/omnifaces/util/Utils.java
@@ -76,7 +76,9 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.concurrent.locks.Lock;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -830,6 +832,55 @@ public final class Utils {
      */
     public static void forEach(Object object, Consumer<? super Object> action) {
         stream(object).forEach(action);
+    }
+
+    // Concurrency ----------------------------------------------------------------------------------------------------
+
+    /**
+     * A {@link FunctionalInterface} to be used with {@link Utils#execAtomic(Lock, Action)}
+     * @since 4.5
+     */
+    @FunctionalInterface
+    public interface Action {
+        void execute() throws Exception;
+    }
+
+    /**
+     * Execute the passed task and return the computed result atomically using the passed lock.
+     * @param lock The {@link Lock} to be used for atomic execution
+     * @param task The {@link FunctionalInterface} to be executed atomically
+     * @since 4.5
+     */
+    public static void execAtomic(Lock lock, Action task) {
+        lock.lock();
+
+        try {
+            task.execute();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Execute the passed task and return the computed result atomically using the passed lock.
+     * @param lock The {@link Lock} to be used for atomic execution
+     * @param task The {@link Supplier} to be executed atomically
+     * @return The result of the passed task.
+     * @since 4.5
+     */
+    public static <R> R execAtomic(Lock lock, Supplier<R> task) {
+        lock.lock();
+
+        try {
+            return task.get();
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     // Dates ----------------------------------------------------------------------------------------------------------
