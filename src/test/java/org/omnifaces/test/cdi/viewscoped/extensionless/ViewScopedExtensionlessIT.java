86266f97cebd6b70b571fc278094c95307013239
Bauke Scholtz <balusc@gmail.com>
2026-04-25 09:47:35 -0400

    IT #952: add copies of ViewScopedIT which uses extensionless mapping

---
+++ src/test/java/org/omnifaces/test/cdi/viewscoped/extensionless/ViewScopedExtensionlessIT.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright OmniFaces
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.test.cdi.viewscoped.extensionless;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.omnifaces.test.OmniFacesIT.WebXml.withThreeViewsInSessionAndExtensionlessMapping;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.omnifaces.test.OmniFacesIT;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class ViewScopedExtensionlessIT extends OmniFacesIT {
+
+    @FindBy(id = "bean")
+    private WebElement bean;
+
+    @FindBy(id = "messages")
+    private WebElement messages;
+
+    @FindBy(id = "unload")
+    private WebElement unload;
+
+    @FindBy(id = "newtab")
+    private WebElement newtab;
+
+    @FindBy(id = "non-ajax:submit")
+    private WebElement nonAjaxSubmit;
+
+    @FindBy(id = "non-ajax:navigate")
+    private WebElement nonAjaxNavigate;
+
+    @FindBy(id = "ajax:submit")
+    private WebElement ajaxSubmit;
+
+    @FindBy(id = "ajax:navigate")
+    private WebElement ajaxNavigate;
+
+    @FindBy(id = "form:conditionallyRenderViewScopedIT")
+    private WebElement conditionallyRenderViewScopedIT;
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return buildWebArchive(ViewScopedExtensionlessIT.class)
+            .withWebXml(withThreeViewsInSessionAndExtensionlessMapping)
+            .createDeployment();
+    }
+
+    @BeforeEach
+    void resetBrowser() {
+        // Make sure browser is crisp clean before starting each test.
+        teardown();
+        setup();
+    }
+
+    @Override
+    protected void open(String pageName) {
+        super.open(pageName == null ? null : pageName.split("\\.")[0]);
+    }
+
+    @Test
+    void nonAjax() {
+        init();
+        assertEquals("init", getMessagesText());
+        var previousBean = bean.getText();
+
+        // Unload.
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Submit then unload.
+        guardHttp(nonAjaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Navigate then unload.
+        guardHttp(nonAjaxNavigate::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("navigate destroy init", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Submit then navigate then unload.
+        guardHttp(nonAjaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardHttp(nonAjaxNavigate::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("navigate destroy init", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Navigate then submit then unload.
+        guardHttp(nonAjaxNavigate::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("navigate destroy init", getMessagesText());
+
+        guardHttp(nonAjaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+    }
+
+    @Test
+    void ajax() {
+        init();
+        assertEquals("init", getMessagesText());
+        var previousBean = bean.getText();
+
+        // Submit then unload.
+        guardAjax(ajaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Navigate then unload.
+        guardAjax(ajaxNavigate::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("navigate destroy init", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Submit then navigate then unload.
+        guardAjax(ajaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardAjax(ajaxNavigate::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("navigate destroy init", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+
+        // Navigate then submit then unload.
+        guardAjax(ajaxNavigate::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("navigate destroy init", getMessagesText());
+
+        guardAjax(ajaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+    }
+
+    @Test
+    void conditionallyRenderViewScopedIT() {
+        open("ViewScopedExtensionlessITWithConditionalRendering");
+        assertEquals("", getMessagesText());
+
+        // Trigger view scoped creation during conditional render.
+        guardAjax(conditionallyRenderViewScopedIT::click);
+        var previousBean = bean.getText();
+        assertEquals("init", getMessagesText());
+
+        // Submit then unload.
+        guardAjax(ajaxSubmit::click);
+        assertEquals(previousBean, previousBean = bean.getText());
+        assertEquals("submit", getMessagesText());
+
+        guardHttp(unload::click);
+        assertNotEquals(previousBean, previousBean = bean.getText());
+        assertEquals("unload init", getMessagesText());
+    }
+
+    @Test
+    void destroyViewState() {
+        destroyViewState("ViewScopedExtensionlessIT");
+    }
+
+    @Test
+    void destroyViewStateWithViewAction() {
+        destroyViewState("ViewScopedExtensionlessITWithViewAction");
+    }
+
+    private void destroyViewState(String pageName) {
+        open(pageName);
+        assertEquals("init", getMessagesText());
+        var firstBean = bean.getText();
+        var firstTab = browser.getWindowHandle();
+
+        // Open three new tabs and close them immediately.
+        openNewTab(newtab);
+        assertEquals("init", getMessagesText());
+        assertNotEquals(firstBean, bean.getText());
+        closeCurrentTabAndSwitchTo(firstTab);
+
+        openNewTab(newtab);
+        assertEquals("unload init", getMessagesText()); // Unload was from previous tab.
+        assertNotEquals(firstBean, bean.getText());
+        closeCurrentTabAndSwitchTo(firstTab);
+
+        openNewTab(newtab);
+        assertEquals("unload init", getMessagesText()); // Unload was from previous tab.
+        assertNotEquals(firstBean, bean.getText());
+        closeCurrentTabAndSwitchTo(firstTab);
+
+        // Submit form in first tab. As Faces is instructed to store only 3 views in session,
+        // and the @ViewScoped unload in three previously opened tabs should also physically
+        // destroy the view state, the submit in first tab should not throw ViewExpiredException.
+        guardAjax(ajaxSubmit::click);
+        assertEquals(firstBean, bean.getText());
+        assertEquals("unload submit", getMessagesText()); // Unload was from previous tab.
+    }
+
+    private String getMessagesText() {
+        return messages.getText().replaceAll("\\s+", " ");
+    }
+
+}
---
+++ src/test/java/org/omnifaces/test/cdi/viewscoped/extensionlessdistributable/ViewScopedExtensionlessDistributableITBean.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright OmniFaces
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.test.cdi.viewscoped.extensionlessdistributable;
+
+import static org.omnifaces.cdi.viewscope.ViewScopeManager.isUnloadRequest;
+import static org.omnifaces.util.Faces.getContext;
+import static org.omnifaces.util.Faces.getViewId;
+import static org.omnifaces.util.Faces.hasContext;
+import static org.omnifaces.util.Messages.addGlobalInfo;
+
+import java.io.Serializable;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.faces.FacesException;
+import jakarta.inject.Named;
+
+import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Hacks;
+
+@Named
+@ViewScoped
+public class ViewScopedExtensionlessDistributableITBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private static boolean unloaded;
+    private static boolean destroyed;
+
+    @PostConstruct
+    public void init() {
+        try {
+            Thread.sleep(500); // Give unload opportunity to hit server.
+        }
+        catch (InterruptedException e) {
+            throw new FacesException(e);
+        }
+
+        checkUnloadedOrDestroyed();
+        addGlobalInfo("init ");
+    }
+
+    public void checkUnloadedOrDestroyed() {
+        if (unloaded) {
+            addGlobalInfo("unload ");
+            unloaded = false;
+        }
+        else if (destroyed) {
+            addGlobalInfo("destroy ");
+            destroyed = false;
+        }
+    }
+
+    public void onload() {
+        // NOOP
+    }
+
+    public void submit() {
+        checkUnloadedOrDestroyed();
+        addGlobalInfo("submit ");
+    }
+
+    public String navigate() {
+        addGlobalInfo("navigate ");
+
+        if (Hacks.isMyFacesUsed()) { // MyFaces refused to fix this. See #4120
+            var renderedResources = Faces.getViewRoot().getTransientStateHelper().getTransient("org.apache.myfaces.RENDERED_RESOURCES_SET");
+            Faces.setViewRoot(Faces.getViewId());
+            Faces.getViewRoot().getTransientStateHelper().putTransient("org.apache.myfaces.RENDERED_RESOURCES_SET", renderedResources);
+            return null;
+        }
+        else {
+            return getViewId();
+        }
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (hasContext() && isUnloadRequest(getContext())) {
+            unloaded = true;
+        }
+        else {
+            destroyed = true;
+        }
+    }
+
+}
