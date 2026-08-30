omnifaces/omnifaces

commit 4684a7a7f7e7da69a739f9dd9b112376272efd23

migrate String#format to String#formatted

src/main/java/org/omnifaces/cdi/push/SocketFacesListener.java
@@ -13,7 +13,6 @@
 package org.omnifaces.cdi.push;
 
 import static jakarta.faces.component.visit.VisitHint.SKIP_ITERATION;
-import static java.lang.String.format;
 import static org.omnifaces.cdi.push.PushChannelManager.ESTIMATED_TOTAL_CHANNELS;
 import static org.omnifaces.util.ComponentsLocal.addScript;
 import static org.omnifaces.util.ComponentsLocal.forEachComponent;
@@ -89,7 +88,7 @@ public class SocketFacesListener implements SystemEventListener {
             boolean previouslyConnected = sockets.get(socket.getChannel()).setValue(connected);
 
             if (connected != previouslyConnected) {
-                addScript(context, format(connected ? SCRIPT_OPEN : SCRIPT_CLOSE, socket.getChannel()));
+                addScript(context, connected ? SCRIPT_OPEN : SCRIPT_CLOSE.formatted(socket.getChannel()));
             }
         });
     }

commit 1d3dcf5c09caad06bf6a6a005dd94a2de0da9dd6

Fix OmniFaces.Push.open() regression in 4684a7a7 #995

src/main/java/org/omnifaces/cdi/push/SocketFacesListener.java
@@ -86,7 +86,7 @@ public class SocketFacesListener implements SystemEventListener {
             boolean previouslyConnected = sockets.get(socket.getChannel()).setValue(connected);
 
             if (connected != previouslyConnected) {
-                addScript(context, connected ? SCRIPT_OPEN : SCRIPT_CLOSE.formatted(socket.getChannel()));
+                addScript(context, (connected ? SCRIPT_OPEN : SCRIPT_CLOSE).formatted(socket.getChannel()));
             }
         });
     }